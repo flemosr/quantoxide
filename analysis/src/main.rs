40a656505bcd8ccd8530117d90a5c1eb4ff82ca1
Rafael Lemos <contact@flemosr.com>
2025-03-08 16:45:03 -0300

    analysis: improve script to download the price history

--- analysis/src/main.rs
+++ analysis/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use std::env;
 use std::{thread, time};
 
@@ -8,6 +9,58 @@ use api::LNMarketsAPI;
 use db::DB;
 
 const LNM_PRICE_HISTORY_LIMIT: usize = 1000;
+// Max LNM REST API rate for public endpoints is 30 requests per minute.
+// Source: https://docs.lnmarkets.com/api/
+const LNM_API_COOLDOWN_SEC: u64 = 3;
+
+async fn download_price_history(
+    lnm_api: &LNMarketsAPI,
+    from: Option<&DateTime<Utc>>,
+    mut to: Option<DateTime<Utc>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        thread::sleep(time::Duration::from_secs(LNM_API_COOLDOWN_SEC));
+        match to {
+            Some(fixed_to) => println!("\nFetching price entries before {fixed_to}..."),
+            None => println!("\nFetching latest price entries..."),
+        }
+
+        let price_history = lnm_api
+            .futures_price_history(None, to, Some(LNM_PRICE_HISTORY_LIMIT))
+            .await?;
+
+        if price_history.len() < LNM_PRICE_HISTORY_LIMIT {
+            panic!(
+                "Received only {} price entries with limit {LNM_PRICE_HISTORY_LIMIT}.",
+                price_history.len()
+            );
+        }
+
+        if let Some(fixed_to) = to {
+            let first_entry_time = *price_history.first().expect("not empty").time();
+            if first_entry_time != fixed_to {
+                panic!("Tried to add price entries without overlap.");
+            }
+        }
+
+        for price_entry in price_history.iter() {
+            if let Some(fixed_from) = from {
+                if *price_entry.time() <= *fixed_from {
+                    // Reached `from` limit
+                    println!("\nReached `from` limit {fixed_from}.");
+                    return Ok(());
+                }
+            }
+
+            match DB.add_price_entry(&price_entry).await? {
+                true => println!("Price entry {:?} was added to the DB.", price_entry),
+                false => println!("Price entry {:?} already existed in the DB.", price_entry),
+            }
+        }
+
+        to = Some(*price_history.last().expect("not empty").time());
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,9 +71,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         env::var("LNM_API_PASSPHRASE").expect("LNM_API_PASSPHRASE must be set");
     let postgres_db_url = env::var("POSTGRES_DB_URL").expect("POSTGRES_DB_URL must be set");
 
-    println!("Trying to init the db...");
+    println!("Trying to init the DB...");
 
     DB.init(&postgres_db_url).await?;
+
+    println!("DB is ready.");
 
     let lnm_api = LNMarketsAPI::new(
         lnm_api_base_url,
@@ -29,94 +84,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         lnm_api_passphrase,
     );
 
-    println!(
-        "Syncing the most recent price entries until reaching the latest price entry in the DB...\n"
-    );
-
     if let Some(latest_price_entry) = DB.get_latest_price_entry().await? {
-        let mut first_fetch = true;
-        let mut get_history_to = chrono::offset::Utc::now();
+        println!(
+            "\nDownloading the latest price entries until reaching the latest ({}) in the DB...",
+            latest_price_entry.time
+        );
 
-        'outer: loop {
-            thread::sleep(time::Duration::from_secs(5));
-            println!("Getting futures price history to {get_history_to}...");
-
-            let price_history = lnm_api
-                .futures_price_history(None, Some(get_history_to), Some(LNM_PRICE_HISTORY_LIMIT))
-                .await?;
-
-            if price_history.len() < LNM_PRICE_HISTORY_LIMIT {
-                panic!(
-                    "Received only {} price entries with limit {LNM_PRICE_HISTORY_LIMIT}",
-                    price_history.len()
-                );
-            }
-
-            if first_fetch == false && *price_history.first().unwrap().time() != get_history_to {
-                panic!("Tried to add entries without overlap");
-            }
-
-            first_fetch = false;
-
-            for price_entry in price_history {
-                match DB.add_price_entry(&price_entry).await? {
-                    true => {
-                        println!("Price entry {:?} was added to the db", price_entry);
-                        get_history_to = *price_entry.time();
-                    }
-                    false => {
-                        println!("Price entry {:?} already existed in the db", price_entry);
-                        if *price_entry.time() < latest_price_entry.time {
-                            // All price entries from this point onwards will be present in the DB.
-                            break 'outer;
-                        }
-                    }
-                }
-            }
-        }
+        download_price_history(&lnm_api, Some(&latest_price_entry.time), None).await?;
     }
 
-    let mut empty_db = true;
-    let mut get_history_to =
-        if let Some(earliest_price_entry) = DB.get_earliest_price_entry().await? {
-            empty_db = false;
+    if let Some(earliest_price_entry) = DB.get_earliest_price_entry().await? {
+        println!(
+            "\nDownloading price entries from the earliest ({}) in the DB backwards...",
             earliest_price_entry.time
-        } else {
-            chrono::offset::Utc::now()
-        };
-
-    println!("\nSyncing older price entries, from the earliest price entry in the DB...\n");
-
-    loop {
-        thread::sleep(time::Duration::from_secs(5));
-        println!("Getting futures price history to {get_history_to}...");
-
-        let price_history = lnm_api
-            .futures_price_history(None, Some(get_history_to), Some(LNM_PRICE_HISTORY_LIMIT))
-            .await?;
-
-        if price_history.len() < LNM_PRICE_HISTORY_LIMIT {
-            panic!(
-                "Received only {} price entries with limit {LNM_PRICE_HISTORY_LIMIT}",
-                price_history.len()
-            );
-        }
-
-        if empty_db == false && *price_history.first().unwrap().time() != get_history_to {
-            panic!("Tried to add entries without overlap");
-        }
-
-        for price_entry in price_history {
-            match DB.add_price_entry(&price_entry).await? {
-                true => {
-                    println!("Price entry {:?} was added to the db", price_entry);
-                    get_history_to = *price_entry.time();
-                }
-                false => {
-                    println!("Price entry {:?} already existed in the db", price_entry);
-                }
-            }
-            empty_db = false;
-        }
+        );
+        download_price_history(&lnm_api, None, Some(earliest_price_entry.time)).await?;
+    } else {
+        println!("\nNo price entries in the DB. Downloading from latest to earliest...");
+        download_price_history(&lnm_api, None, None).await?;
     }
+
+    Ok(())
 }
