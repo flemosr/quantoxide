6b683ea659506253ba21ed381ec2229694f5d743
Rafael Lemos <contact@flemosr.com>
2025-03-10 08:29:06 -0300

    analysis: fix `create_price_history_table` migration synthax

--- analysis/migrations/20250309201627_create_price_history_table.sql
+++ analysis/migrations/20250309201627_create_price_history_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS price_history (
     time TIMESTAMPTZ NOT NULL UNIQUE,
     value DOUBLE PRECISION NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    next TIMESTAMPTZ UNIQUE,
+    next TIMESTAMPTZ UNIQUE
 );
 
 -- Add index on time for faster time-based queries
