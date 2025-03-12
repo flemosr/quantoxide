b9c13bf9c60c41e71e878007d588a604de9c4cf5
Rafael Lemos <contact@flemosr.com>
2025-03-12 11:10:48 -0300

    analysis: adjust `Database::add_price_entry`
    Consider `price_history_locf` updates.

--- analysis/src/db.rs
+++ analysis/src/db.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, SubsecRound, Utc};
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use tokio::sync::OnceCell;
 
@@ -117,25 +117,94 @@ impl Database {
         }
     }
 
+    fn get_locf_sec(time: &DateTime<Utc>) -> DateTime<Utc> {
+        let trunc_time_sec = time.trunc_subsecs(0);
+        if trunc_time_sec == *time {
+            trunc_time_sec
+        } else {
+            trunc_time_sec + Duration::seconds(1)
+        }
+    }
+
     pub async fn add_price_entry(
         &self,
         price_entry: &PriceEntryLNM,
         next: Option<&DateTime<Utc>>,
     ) -> Result<bool, sqlx::Error> {
-        let pool = self.get_pool();
-        let query = "INSERT INTO price_history (time, value, next) 
-                     VALUES ($1, $2, $3) 
-                     ON CONFLICT (time) 
-                     DO NOTHING";
+        let mut tx = self.get_pool().begin().await?;
+        let query = r#"
+            INSERT INTO price_history (time, value, next) 
+            VALUES ($1, $2, $3) 
+            ON CONFLICT (time) 
+            DO NOTHING
+        "#;
 
         let result = sqlx::query(query)
             .bind(price_entry.time())
             .bind(price_entry.value())
             .bind(next)
-            .execute(pool)
+            .execute(&mut *tx)
             .await?;
 
-        Ok(result.rows_affected() > 0)
+        if result.rows_affected() == 0 {
+            return Ok(false);
+        }
+
+        // We can assume that `price_history_locf` is up-to-date in regards to the previously
+        // added price entries.
+        // A new entry will potentially affect the values of all locf entries between its own
+        // `locf_sec` and the `locf_sec` corresponding to the observed entry just AFTER it.
+        // If there are no observed entries after the just-added entry, only the value of the locf
+        // entry corresponding to `locf_sec` will potentially be affected.
+        // A new entry added after the max locf time, or before the min locf time may result in
+        // gaps in the `price_history_locf` history. Said gaps need to be filled carrying the
+        // corresponding locf value onwards or backwards.
+
+        let added_entry_time = price_entry.time();
+        let added_locf_sec = Self::get_locf_sec(added_entry_time);
+
+        let prev_locf_sec: Option<DateTime<Utc>> =
+            sqlx::query_scalar("SELECT max(time) FROM price_history_locf WHERE time <= $1")
+                .bind(added_entry_time)
+                .fetch_one(&mut *tx)
+                .await?;
+        // `prev_locf_sec` will be `None` only when `added_locf_sec` is the new min locf time
+        let start_locf_sec = prev_locf_sec.unwrap_or(added_locf_sec);
+
+        let next_observed_entry_time: Option<DateTime<Utc>> =
+            sqlx::query_scalar("SELECT min(time) FROM price_history WHERE time > $1")
+                .bind(added_entry_time)
+                .fetch_one(&mut *tx)
+                .await?;
+        let end_locf_sec = if let Some(next_observed_entry_time) = next_observed_entry_time {
+            Self::get_locf_sec(&next_observed_entry_time)
+        } else {
+            added_locf_sec
+        };
+
+        let query = r#"
+            INSERT INTO price_history_locf (time, value)
+            SELECT s.time, t.value
+            FROM generate_series($1, $2, '1 second'::interval) AS s(time)
+            LEFT JOIN LATERAL (
+                SELECT value
+                FROM price_history
+                WHERE time <= s.time
+                ORDER BY time DESC
+                LIMIT 1
+            ) t ON true
+            ON CONFLICT (time) 
+            DO UPDATE SET value = EXCLUDED.value
+        "#;
+        sqlx::query(query)
+            .bind(start_locf_sec)
+            .bind(end_locf_sec)
+            .execute(&mut *tx)
+            .await?;
+
+        tx.commit().await?;
+
+        Ok(true)
     }
 
     pub async fn update_price_entry_next(
