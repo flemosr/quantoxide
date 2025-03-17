a8772d3a4ffed04083dad7f3921c085e97499cb5
Rafael Lemos <contact@flemosr.com>
2025-03-17 11:29:41 -0300

    analysis: adjust migrations, add `alter_price_history_locf_table`
    Remove `create_indicators_table`.

---
+++ analysis/migrations/20250317140406_alter_price_history_locf_table.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE price_history_locf
+    DROP COLUMN ma_5,
+    DROP COLUMN ma_60,
+    DROP COLUMN ma_300;
---
+++ analysis/migrations/20250317140406_alter_price_history_locf_table.up.sql
@@ -0,0 +1,37 @@
+ALTER TABLE price_history_locf
+    ADD COLUMN ma_5 DOUBLE PRECISION,
+    ADD COLUMN ma_60 DOUBLE PRECISION,
+    ADD COLUMN ma_300 DOUBLE PRECISION;
+
+WITH price_data AS (
+    SELECT time, value, ROW_NUMBER() OVER (ORDER BY time) AS rn
+    FROM price_history_locf
+    ORDER BY time ASC
+),
+eval_indicators AS (
+    SELECT
+        time,
+        CASE
+            WHEN rn >= 5
+            THEN AVG(value) OVER (ORDER BY time ASC ROWS BETWEEN 4 PRECEDING AND CURRENT ROW)
+            ELSE NULL
+        END AS ma_5,
+        CASE
+            WHEN rn >= 60
+            THEN AVG(value) OVER (ORDER BY time ASC ROWS BETWEEN 59 PRECEDING AND CURRENT ROW)
+            ELSE NULL
+        END AS ma_60,
+        CASE
+            WHEN rn >= 300
+            THEN AVG(value) OVER (ORDER BY time ASC ROWS BETWEEN 299 PRECEDING AND CURRENT ROW)
+            ELSE NULL
+        END AS ma_300
+    FROM price_data
+)
+UPDATE price_history_locf
+SET
+    ma_5 = eval_indicators.ma_5,
+    ma_60 = eval_indicators.ma_60,
+    ma_300 = eval_indicators.ma_300
+FROM eval_indicators
+WHERE price_history_locf.time = eval_indicators.time;
