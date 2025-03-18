7554e8502658e2df647b2488861e59f182e2ff44
Rafael Lemos <contact@flemosr.com>
2025-03-18 14:13:32 -0300

    analysis: add `PriceEntryLOCF` model

--- analysis/src/db/models.rs
+++ analysis/src/db/models.rs
@@ -8,3 +8,12 @@ pub struct PriceEntry {
     pub created_at: DateTime<Utc>,
     pub next: Option<DateTime<Utc>>,
 }
+
+#[derive(Debug, FromRow)]
+pub struct PriceEntryLOCF {
+    pub time: DateTime<Utc>,
+    pub value: f64,
+    pub ma_5: Option<f64>,
+    pub ma_60: Option<f64>,
+    pub ma_300: Option<f64>,
+}
