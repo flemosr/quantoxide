71b6d1f5baa65e130b20cc58eb9f87c30042a85a
Rafael Lemos <contact@flemosr.com>
2025-03-21 10:09:11 -0300

    analysis: adjust `api`

--- analysis/src/api.rs
+++ analysis/src/api.rs
@@ -4,9 +4,6 @@ use serde::Deserialize;
 
 use crate::env::LNM_API_BASE_URL;
 
-#[derive(Debug)]
-pub struct LNMarketsAPI;
-
 #[derive(Debug, Deserialize)]
 pub struct PriceEntryLNM {
     #[serde(with = "ts_milliseconds")]
@@ -24,36 +21,29 @@ impl PriceEntryLNM {
     }
 }
 
-impl LNMarketsAPI {
-    pub fn new() -> Self {
-        Self
+const FUTURES_PRICE_HISTORY_PATH: &'static str = "/futures/history/price";
+
+pub async fn futures_price_history(
+    from: Option<DateTime<Utc>>,
+    to: Option<DateTime<Utc>>,
+    limit: Option<usize>,
+) -> Result<Vec<PriceEntryLNM>, Box<dyn std::error::Error>> {
+    let mut params = Vec::new();
+    if let Some(from) = from {
+        params.push(("from", from.timestamp_millis().to_string()));
+    }
+    if let Some(to) = to {
+        params.push(("to", to.timestamp_millis().to_string()));
+    }
+    if let Some(limit) = limit {
+        params.push(("limit", limit.to_string()));
     }
 
-    const FUTURES_PRICE_HISTORY_PATH: &'static str = "/futures/history/price";
+    let endpoint = LNM_API_BASE_URL.clone() + FUTURES_PRICE_HISTORY_PATH;
+    let url = reqwest::Url::parse_with_params(&endpoint, params)?;
+    let res = reqwest::get(url).await?;
 
-    pub async fn futures_price_history(
-        &self,
-        from: Option<DateTime<Utc>>,
-        to: Option<DateTime<Utc>>,
-        limit: Option<usize>,
-    ) -> Result<Vec<PriceEntryLNM>, Box<dyn std::error::Error>> {
-        let mut params = Vec::new();
-        if let Some(from) = from {
-            params.push(("from", from.timestamp_millis().to_string()));
-        }
-        if let Some(to) = to {
-            params.push(("to", to.timestamp_millis().to_string()));
-        }
-        if let Some(limit) = limit {
-            params.push(("limit", limit.to_string()));
-        }
+    let price_history = res.json::<Vec<PriceEntryLNM>>().await?;
 
-        let endpoint = LNM_API_BASE_URL.clone() + Self::FUTURES_PRICE_HISTORY_PATH;
-        let url = reqwest::Url::parse_with_params(&endpoint, params)?;
-        let res = reqwest::get(url).await?;
-
-        let price_history = res.json::<Vec<PriceEntryLNM>>().await?;
-
-        Ok(price_history)
-    }
+    Ok(price_history)
 }
