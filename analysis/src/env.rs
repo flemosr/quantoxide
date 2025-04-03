4324eb7bd52943cd7ec60eff3ca16972670e39a6
Rafael Lemos <contact@flemosr.com>
2025-04-03 09:36:03 -0300

    analysis: adjust `env`
    Rename `LNM_MIN_PRICE_HISTORY_WEEKS` to `SYNC_HISTORY_REACH_WEEKS`,
    refactor.

--- analysis/src/env.rs
+++ analysis/src/env.rs
@@ -10,43 +10,37 @@ lazy_static! {
     pub static ref LNM_API_PASSPHRASE: String =
         env::var("LNM_API_PASSPHRASE").expect("LNM_API_PASSPHRASE must be set");
     pub static ref LNM_PRICE_HISTORY_LIMIT: usize = {
-        let var = env::var("LNM_PRICE_HISTORY_LIMIT").expect("LNM_PRICE_HISTORY_LIMIT must be set");
-        let num = var
+        let num = env::var("LNM_PRICE_HISTORY_LIMIT")
+            .expect("LNM_PRICE_HISTORY_LIMIT must be set")
             .parse::<usize>()
             .expect("LNM_PRICE_HISTORY_LIMIT must be a valid number");
         assert!(num >= 2, "LNM_PRICE_HISTORY_LIMIT must be at least 2");
         num
     };
     pub static ref LNM_API_COOLDOWN_SEC: u64 = {
-        let var = env::var("LNM_API_COOLDOWN_SEC").expect("LNM_API_COOLDOWN_SEC must be set");
-        
-        var
+        env::var("LNM_API_COOLDOWN_SEC")
+            .expect("LNM_API_COOLDOWN_SEC must be set")
             .parse::<u64>()
             .expect("LNM_API_COOLDOWN_SEC must be a valid number")
     };
     pub static ref LNM_API_ERROR_MAX_TRIALS: u32 = {
-        let var =
-            env::var("LNM_API_ERROR_MAX_TRIALS").expect("LNM_API_ERROR_MAX_TRIALS must be set");
-        
-        var
+        env::var("LNM_API_ERROR_MAX_TRIALS")
+            .expect("LNM_API_ERROR_MAX_TRIALS must be set")
             .parse::<u32>()
             .expect("LNM_API_ERROR_MAX_TRIALS must be a valid number")
     };
     pub static ref LNM_API_ERROR_COOLDOWN_SEC: u64 = {
-        let var =
-            env::var("LNM_API_ERROR_COOLDOWN_SEC").expect("LNM_API_ERROR_COOLDOWN_SEC must be set");
-        
-        var
+        env::var("LNM_API_ERROR_COOLDOWN_SEC")
+            .expect("LNM_API_ERROR_COOLDOWN_SEC must be set")
             .parse::<u64>()
             .expect("LNM_API_ERROR_COOLDOWN_SEC must be a valid number")
     };
-    pub static ref LNM_MIN_PRICE_HISTORY_WEEKS: u64 = {
-        let var = env::var("LNM_MIN_PRICE_HISTORY_WEEKS")
-            .expect("LNM_MIN_PRICE_HISTORY_WEEKS must be set");
-        let num = var
+    pub static ref SYNC_HISTORY_REACH_WEEKS: u64 = {
+        let num = env::var("SYNC_HISTORY_REACH_WEEKS")
+            .expect("SYNC_HISTORY_REACH_WEEKS must be set")
             .parse::<u64>()
-            .expect("LNM_MIN_PRICE_HISTORY_WEEKS must be a valid number");
-        assert!(num >= 1, "LNM_MIN_PRICE_HISTORY_WEEKS must be at least 1");
+            .expect("SYNC_HISTORY_REACH_WEEKS must be a valid number");
+        assert!(num >= 1, "SYNC_HISTORY_REACH_WEEKS must be at least 1");
         num
     };
     pub static ref POSTGRES_DB_URL: String =
