c812125c0368be991398980b28205d97377832d2
Rafael Lemos <contact@flemosr.com>
2025-04-08 11:28:12 -0300

    analysis: refactor `sync`, reorganize mod

--- analysis/src/sync/mod.rs
+++ analysis/src/sync/mod.rs
@@ -1,15 +1,13 @@
 use chrono::{DateTime, Duration, Utc};
-use price_history_state::PriceHistoryState;
-use std::{collections::HashSet, sync::Arc};
+use std::sync::Arc;
 use tokio::{
-    sync::{broadcast, Mutex},
+    sync::{broadcast, mpsc::Receiver, Mutex},
     task::JoinHandle,
     time,
 };
 
 use crate::{
     api::{
-        rest::models::PriceEntryLNM,
         websocket::models::{ConnectionState, LnmWebSocketChannel, WebSocketApiRes},
         ApiContext,
     },
@@ -17,7 +15,9 @@ use crate::{
     error::{AppError, Result},
 };
 
-pub mod price_history_state;
+mod price_history_task;
+
+use price_history_task::{HistoryStateReceiver, PriceHistoryState, SyncPriceHistoryTask};
 
 pub type SyncTransmiter = broadcast::Sender<SyncState>;
 pub type SyncReceiver = broadcast::Receiver<SyncState>;
@@ -52,8 +52,7 @@ impl Sync {
         db: Arc<DbContext>,
         api: Arc<ApiContext>,
     ) -> Self {
-        // let sync_reach = Utc::now() - Duration::weeks(sync_history_reach_weeks as i64);
-        let sync_reach = Utc::now() - Duration::days(2);
+        let sync_reach = Utc::now() - Duration::weeks(sync_history_reach_weeks as i64);
 
         // External channel for sync updates
         let (sync_tx, _) = broadcast::channel::<SyncState>(100);
@@ -88,164 +87,16 @@ impl Sync {
         Ok(())
     }
 
-    async fn handle_price_history_update(
-        &self,
-        new_history_state: &PriceHistoryState,
-    ) -> Result<()> {
-        let sync_state = self.sync_state.lock().await;
-        if let SyncState::InProgress(_) = *sync_state {
-            let new_sync_state = SyncState::InProgress(new_history_state.clone());
-            self.handle_sync_state_update(new_sync_state).await?;
-        }
-        Ok(())
-    }
-
-    async fn get_new_price_entries(
-        &self,
-        from_observed_time: Option<&DateTime<Utc>>,
-        to_observed_time: Option<&DateTime<Utc>>,
-    ) -> Result<(Vec<PriceEntryLNM>, bool)> {
-        let mut price_entries = {
-            let mut trials = 0;
-            let rest_futures = &self.api.rest().futures;
-            loop {
-                time::sleep(self.api_cooldown).await;
-
-                match rest_futures
-                    .price_history(None, to_observed_time, Some(self.api_history_max_entries))
-                    .await
-                {
-                    Ok(price_entries) => break price_entries,
-                    Err(api_error) => {
-                        trials += 1;
-                        if trials >= self.api_error_max_trials {
-                            return Err(AppError::ApiMaxTrialsReached {
-                                api_error,
-                                max_trials: self.api_error_max_trials,
-                            });
-                        }
-
-                        time::sleep(self.api_error_cooldown).await;
-                        continue;
-                    }
-                };
-            }
-        };
-
-        // Remove entries with duplicated 'time'
-        let mut seen = HashSet::new();
-        price_entries.retain(|price_entry| seen.insert(*price_entry.time()));
-
-        let is_sorted_time_desc = price_entries.is_sorted_by(|a, b| a.time() > b.time());
-        if !is_sorted_time_desc {
-            return Err(AppError::UnexpectedLNMPayload(
-                "Price entries unsorted by time desc".to_string(),
-            ));
-        }
-
-        // If `before_observed_time` is set, ensure that the first (latest) entry matches it
-        if let Some(observed_time) = to_observed_time {
-            let first_entry = price_entries.remove(0);
-            if first_entry.time() != observed_time {
-                return Err(AppError::UnexpectedLNMPayload(
-                    "Price entries without overlap".to_string(),
-                ));
-            }
-        }
-
-        let from_observed_time_received = if let Some(time) = from_observed_time {
-            if let Some(entry_i) = price_entries
-                .iter()
-                .position(|price_entry| price_entry.time() <= time)
-            {
-                // Remove the entries before the `limit`
-                let before_limit = price_entries.split_off(entry_i);
-                let overlap = before_limit.first().expect("not empty").time() == time;
-
-                if !overlap {
-                    return Err(AppError::UnexpectedLNMPayload(format!(
-                        "limit entry time {} not received from server",
-                        time
-                    )));
-                }
-
-                true
-            } else {
-                false
-            }
-        } else {
-            false
-        };
-
-        Ok((price_entries, from_observed_time_received))
-    }
-
-    async fn partial_download(
-        &self,
-        from_observed_time: Option<&DateTime<Utc>>,
-        to_observed_time: Option<&DateTime<Utc>>,
-    ) -> Result<bool> {
-        let (new_price_entries, from_observed_time_received) = self
-            .get_new_price_entries(from_observed_time, to_observed_time)
-            .await?;
-
-        if !new_price_entries.is_empty() {
-            self.db
-                .price_history
-                .add_entries(&new_price_entries, to_observed_time)
-                .await?;
-        }
-
-        if from_observed_time_received {
-            // `next` property of `from_observed_time` entry needs to be updated
-
-            let next_observed_time = if let Some(earliest_new_entry) = new_price_entries.last() {
-                Some(*earliest_new_entry.time())
-            } else if let Some(time) = to_observed_time {
-                // If there is a `next_observed_time`, the first entry received from the server
-                // matched its time (upper overlap enforcement).
-                // From this, we can infer that there are no entries to be fetched between
-                // `from_observed_time` and `next_observed_time` (edge case).
-                Some(*time)
-            } else {
-                // No entries available after `from_observed_time`
-                None
-            };
-
-            if let Some(next) = next_observed_time {
-                self.db
-                    .price_history
-                    .update_entry_next(from_observed_time.expect("from received"), &next)
-                    .await?;
-            }
-        }
-
-        Ok(!new_price_entries.is_empty())
-    }
-
-    async fn sync_price_history(&self) -> Result<()> {
-        let mut history_state = PriceHistoryState::evaluate(&self.db, self.sync_reach).await?;
-        self.handle_price_history_update(&history_state).await?;
-
-        loop {
-            let (download_from, download_to) = history_state.next_download_bounds();
-
-            let new_entries_received = self.partial_download(download_from, download_to).await?;
-            if !new_entries_received {
-                if history_state.has_gaps() {
-                    return Err(AppError::Generic(
-                        "no entries received while gaps still exist".to_string(),
-                    ));
-                } else {
-                    break;
-                }
-            }
-
-            history_state = PriceHistoryState::evaluate(&self.db, self.sync_reach).await?;
-            self.handle_price_history_update(&history_state).await?;
-        }
-
-        Ok(())
+    fn price_history_task(&self) -> (SyncPriceHistoryTask, HistoryStateReceiver) {
+        SyncPriceHistoryTask::new(
+            self.api_cooldown,
+            self.api_error_cooldown,
+            self.api_error_max_trials,
+            self.api_history_max_entries,
+            self.sync_reach,
+            self.db.clone(),
+            self.api.clone(),
+        )
     }
 
     async fn start_real_time_collection(&self) -> Result<JoinHandle<()>> {
@@ -285,19 +136,57 @@ impl Sync {
         Ok(handle)
     }
 
+    fn handle_history_state_updates(&self, mut history_state_rx: Receiver<PriceHistoryState>) {
+        let sync_state = self.sync_state.clone();
+        let sync_tx = self.sync_tx.clone();
+        tokio::spawn(async move {
+            while let Some(new_history_state) = history_state_rx.recv().await {
+                let mut sync_state = sync_state.lock().await;
+                if let SyncState::InProgress(_) = *sync_state {
+                    let new_sync_state = SyncState::InProgress(new_history_state);
+                    *sync_state = new_sync_state.clone();
+                    if sync_tx.receiver_count() > 0 {
+                        let _ = sync_tx.send(new_sync_state).map_err(|_| {
+                            AppError::Generic("couldn't send sync update".to_string())
+                        });
+                    }
+                }
+            }
+        });
+    }
     pub async fn start(&self) -> Result<()> {
         // Initial price history sync
-        self.sync_price_history().await?;
+        let (sync_price_history_task, history_state_rx) = self.price_history_task();
+
+        self.handle_history_state_updates(history_state_rx);
+
+        sync_price_history_task.run().await?;
 
         // Start to collect real-time data
-        let _real_time_collection_handle = self.start_real_time_collection().await?;
+        let mut real_time_collection_handle = self.start_real_time_collection().await?;
 
         // Additional price history sync to ensure overlap with real-time data
-        self.sync_price_history().await?;
+        let (sync_price_history_task, history_state_rx) = self.price_history_task();
+
+        self.handle_history_state_updates(history_state_rx);
+
+        sync_price_history_task.run().await?;
 
         // Sync achieved
         self.handle_sync_state_update(SyncState::Synced).await?;
 
-        Ok(())
+        loop {
+            let (sync_price_history_task, _) = self.price_history_task();
+
+            tokio::select! {
+                res = &mut real_time_collection_handle => {
+
+                }
+                _ = time::sleep(time::Duration::from_secs(60)) => {
+                    sync_price_history_task.run().await?;
+                    continue;
+                }
+            }
+        }
     }
 }
---
+++ analysis/src/sync/price_history_task/mod.rs
@@ -0,0 +1,212 @@
+use chrono::{DateTime, Utc};
+use std::{collections::HashSet, sync::Arc};
+use tokio::{sync::mpsc, time};
+
+use crate::{
+    api::{rest::models::PriceEntryLNM, ApiContext},
+    db::DbContext,
+    error::{AppError, Result},
+};
+
+mod price_history_state;
+pub use price_history_state::PriceHistoryState;
+
+pub type HistoryStateTransmiter = mpsc::Sender<PriceHistoryState>;
+pub type HistoryStateReceiver = mpsc::Receiver<PriceHistoryState>;
+
+#[derive(Clone)]
+pub struct SyncPriceHistoryTask {
+    api_cooldown: time::Duration,
+    api_error_cooldown: time::Duration,
+    api_error_max_trials: u32,
+    api_history_max_entries: usize,
+    sync_reach: DateTime<Utc>,
+    db: Arc<DbContext>,
+    api: Arc<ApiContext>,
+    history_state_tx: HistoryStateTransmiter,
+}
+
+impl SyncPriceHistoryTask {
+    pub fn new(
+        api_cooldown: time::Duration,
+        api_error_cooldown: time::Duration,
+        api_error_max_trials: u32,
+        api_history_max_entries: usize,
+        sync_reach: DateTime<Utc>,
+        db: Arc<DbContext>,
+        api: Arc<ApiContext>,
+    ) -> (Self, HistoryStateReceiver) {
+        // External channel for sync updates
+        let (history_state_tx, history_state_rx) = mpsc::channel::<PriceHistoryState>(100);
+
+        let task = Self {
+            api_cooldown,
+            api_error_cooldown,
+            api_error_max_trials,
+            api_history_max_entries,
+            sync_reach,
+            db,
+            api,
+            history_state_tx,
+        };
+
+        (task, history_state_rx)
+    }
+
+    async fn get_new_price_entries(
+        &self,
+        from_observed_time: Option<&DateTime<Utc>>,
+        to_observed_time: Option<&DateTime<Utc>>,
+    ) -> Result<(Vec<PriceEntryLNM>, bool)> {
+        let mut price_entries = {
+            let mut trials = 0;
+            let rest_futures = &self.api.rest().futures;
+            loop {
+                time::sleep(self.api_cooldown).await;
+
+                match rest_futures
+                    .price_history(None, to_observed_time, Some(self.api_history_max_entries))
+                    .await
+                {
+                    Ok(price_entries) => break price_entries,
+                    Err(api_error) => {
+                        trials += 1;
+                        if trials >= self.api_error_max_trials {
+                            return Err(AppError::ApiMaxTrialsReached {
+                                api_error,
+                                max_trials: self.api_error_max_trials,
+                            });
+                        }
+
+                        time::sleep(self.api_error_cooldown).await;
+                        continue;
+                    }
+                };
+            }
+        };
+
+        // Remove entries with duplicated 'time'
+        let mut seen = HashSet::new();
+        price_entries.retain(|price_entry| seen.insert(*price_entry.time()));
+
+        let is_sorted_time_desc = price_entries.is_sorted_by(|a, b| a.time() > b.time());
+        if !is_sorted_time_desc {
+            return Err(AppError::UnexpectedLNMPayload(
+                "Price entries unsorted by time desc".to_string(),
+            ));
+        }
+
+        // If `before_observed_time` is set, ensure that the first (latest) entry matches it
+        if let Some(observed_time) = to_observed_time {
+            let first_entry = price_entries.remove(0);
+            if first_entry.time() != observed_time {
+                return Err(AppError::UnexpectedLNMPayload(
+                    "Price entries without overlap".to_string(),
+                ));
+            }
+        }
+
+        let from_observed_time_received = if let Some(time) = from_observed_time {
+            if let Some(entry_i) = price_entries
+                .iter()
+                .position(|price_entry| price_entry.time() <= time)
+            {
+                // Remove the entries before the `limit`
+                let before_limit = price_entries.split_off(entry_i);
+                let overlap = before_limit.first().expect("not empty").time() == time;
+
+                if !overlap {
+                    return Err(AppError::UnexpectedLNMPayload(format!(
+                        "limit entry time {} not received from server",
+                        time
+                    )));
+                }
+
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        Ok((price_entries, from_observed_time_received))
+    }
+
+    async fn partial_download(
+        &self,
+        from_observed_time: Option<&DateTime<Utc>>,
+        to_observed_time: Option<&DateTime<Utc>>,
+    ) -> Result<bool> {
+        let (new_price_entries, from_observed_time_received) = self
+            .get_new_price_entries(from_observed_time, to_observed_time)
+            .await?;
+
+        if !new_price_entries.is_empty() {
+            self.db
+                .price_history
+                .add_entries(&new_price_entries, to_observed_time)
+                .await?;
+        }
+
+        if from_observed_time_received {
+            // `next` property of `from_observed_time` entry needs to be updated
+
+            let next_observed_time = if let Some(earliest_new_entry) = new_price_entries.last() {
+                Some(*earliest_new_entry.time())
+            } else if let Some(time) = to_observed_time {
+                // If there is a `next_observed_time`, the first entry received from the server
+                // matched its time (upper overlap enforcement).
+                // From this, we can infer that there are no entries to be fetched between
+                // `from_observed_time` and `next_observed_time` (edge case).
+                Some(*time)
+            } else {
+                // No entries available after `from_observed_time`
+                None
+            };
+
+            if let Some(next) = next_observed_time {
+                self.db
+                    .price_history
+                    .update_entry_next(from_observed_time.expect("from received"), &next)
+                    .await?;
+            }
+        }
+
+        Ok(!new_price_entries.is_empty())
+    }
+
+    async fn handle_history_update(&self, new_history_state: &PriceHistoryState) -> Result<()> {
+        if !self.history_state_tx.is_closed() {
+            self.history_state_tx
+                .send(new_history_state.clone())
+                .await
+                .map_err(|_| AppError::Generic("couldn't send price history update".to_string()))?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let mut history_state = PriceHistoryState::evaluate(&self.db, self.sync_reach).await?;
+        self.handle_history_update(&history_state).await?;
+
+        loop {
+            let (download_from, download_to) = history_state.next_download_bounds();
+
+            let new_entries_received = self.partial_download(download_from, download_to).await?;
+            if !new_entries_received {
+                if history_state.has_gaps() {
+                    return Err(AppError::Generic(
+                        "no entries received while gaps still exist".to_string(),
+                    ));
+                } else {
+                    return Ok(());
+                }
+            }
+
+            history_state = PriceHistoryState::evaluate(&self.db, self.sync_reach).await?;
+            self.handle_history_update(&history_state).await?;
+        }
+    }
+}
