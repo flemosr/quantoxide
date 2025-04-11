e695608dd40cb1b46d8dcf1c57dd7fb658740d51
Rafael Lemos <contact@flemosr.com>
2025-04-11 09:22:09 -0300

    analysis: adjust `AppError`, remove deprecated values

--- analysis/src/error.rs
+++ analysis/src/error.rs
@@ -1,38 +1,14 @@
-use chrono::{DateTime, Utc};
 use std::result;
 use thiserror::Error;
 
-use crate::{
-    api::{error::ApiError, websocket::error::WebSocketApiError},
-    db::error::DbError,
-};
+use crate::{db::error::DbError, sync::error::SyncError};
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    #[error("Sync error: {0}")]
+    Sync(#[from] SyncError),
     #[error("Database error: {0}")]
     Db(#[from] DbError),
-    #[error("API error: {0}")]
-    Api(#[from] ApiError),
-    #[error("API Max Trials reached error. Api Error: {api_error}, Max Trials: {max_trials}")]
-    ApiMaxTrialsReached {
-        api_error: ApiError,
-        max_trials: u32,
-    },
-    #[error("Unexpected LNM payload error: {0}")]
-    UnexpectedLNMPayload(String),
-    #[error("Unreachable DB gap error. Earliest Gap: {gap}, Reach {reach}")]
-    UnreachableDbGap {
-        gap: DateTime<Utc>,
-        reach: DateTime<Utc>,
-    },
-    #[error("App generic error: {0}")]
-    Generic(String),
-}
-
-impl From<WebSocketApiError> for AppError {
-    fn from(err: WebSocketApiError) -> Self {
-        AppError::Api(ApiError::from(err))
-    }
 }
 
 pub type Result<T> = result::Result<T, AppError>;
