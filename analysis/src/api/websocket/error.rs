2c5f30023a844310e4525a804fc7dd43802eca09
Rafael Lemos <contact@flemosr.com>
2025-04-12 11:54:43 -0300

    analysis: adjust `WebSocketApiError`, add new error types

--- analysis/src/api/websocket/error.rs
+++ analysis/src/api/websocket/error.rs
@@ -1,5 +1,8 @@
-use std::{result, sync::Arc};
+use fastwebsockets::{OpCode, WebSocketError};
+use hyper::http;
+use std::{io, result, string::FromUtf8Error, sync::Arc};
 use thiserror::Error;
+use tokio_rustls::rustls::pki_types::InvalidDnsNameError;
 
 use super::models::ConnectionState;
 
@@ -7,6 +10,28 @@ use super::models::ConnectionState;
 pub enum WebSocketApiError {
     #[error("BadConnectionState error")]
     BadConnectionState(Arc<ConnectionState>),
+    #[error("InvalidDnsName error")]
+    InvalidDnsName(InvalidDnsNameError),
+    #[error("CreateTcpStream error")]
+    CreateTcpStream(io::Error),
+    #[error("ConnectTcpStream error")]
+    ConnectTcpStream(io::Error),
+    #[error("HttpUpgradeRequest error")]
+    HttpUpgradeRequest(http::Error),
+    #[error("Handshake error")]
+    Handshake(WebSocketError),
+    #[error("WriteFrame error")]
+    WriteFrame(WebSocketError),
+    #[error("EncodeJson error")]
+    EncodeJson(serde_json::Error),
+    #[error("ReadFrame error")]
+    ReadFrame(WebSocketError),
+    #[error("DecodeText error")]
+    DecodeText(FromUtf8Error),
+    #[error("DecodeJson error")]
+    DecodeJson(serde_json::Error),
+    #[error("UnhandledOpCode error")]
+    UnhandledOpCode(OpCode),
     #[error("WebSocket generic error: {0}")]
     Generic(String),
 }
