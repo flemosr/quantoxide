e3fc5169c2b1ae471b5ee50e87913813ddc217d0
Rafael Lemos <contact@flemosr.com>
2025-04-12 12:16:36 -0300

    analysis: adjust `WebSocketApiError`
    Add new error types, improve display.

--- analysis/src/api/websocket/error.rs
+++ analysis/src/api/websocket/error.rs
@@ -2,36 +2,49 @@ use fastwebsockets::{OpCode, WebSocketError};
 use hyper::http;
 use std::{io, result, string::FromUtf8Error, sync::Arc};
 use thiserror::Error;
+use tokio::sync::broadcast::error::SendError;
 use tokio_rustls::rustls::pki_types::InvalidDnsNameError;
 
-use super::models::ConnectionState;
+use super::models::{ConnectionState, WebSocketApiRes};
 
 #[derive(Error, Debug)]
 pub enum WebSocketApiError {
-    #[error("BadConnectionState error")]
+    #[error("BadConnectionState error, {0:?}")]
     BadConnectionState(Arc<ConnectionState>),
-    #[error("InvalidDnsName error")]
+    #[error("InvalidDnsName error, {0}")]
     InvalidDnsName(InvalidDnsNameError),
-    #[error("CreateTcpStream error")]
+    #[error("CreateTcpStream error, {0}")]
     CreateTcpStream(io::Error),
-    #[error("ConnectTcpStream error")]
+    #[error("ConnectTcpStream error, {0}")]
     ConnectTcpStream(io::Error),
-    #[error("HttpUpgradeRequest error")]
+    #[error("HttpUpgradeRequest error, {0}")]
     HttpUpgradeRequest(http::Error),
-    #[error("Handshake error")]
+    #[error("Handshake error, {0}")]
     Handshake(WebSocketError),
-    #[error("WriteFrame error")]
+    #[error("WriteFrame error, {0}")]
     WriteFrame(WebSocketError),
-    #[error("EncodeJson error")]
+    #[error("EncodeJson error, {0}")]
     EncodeJson(serde_json::Error),
-    #[error("ReadFrame error")]
+    #[error("ReadFrame error, {0}")]
     ReadFrame(WebSocketError),
-    #[error("DecodeText error")]
+    #[error("DecodeText error, {0}")]
     DecodeText(FromUtf8Error),
-    #[error("DecodeJson error")]
+    #[error("DecodeJson error, {0}")]
     DecodeJson(serde_json::Error),
-    #[error("UnhandledOpCode error")]
+    #[error("UnhandledOpCode error, {0:?}")]
     UnhandledOpCode(OpCode),
+    #[error("SubscriptionConfirmation error")]
+    SubscriptionConfirmation,
+    #[error("SubscriptionMessage error")]
+    SubscriptionMessage(SendError<WebSocketApiRes>),
+    #[error("ServerRequestedShutdown error")]
+    ServerRequestedShutdown,
+    #[error("NoShutdownConfirmation error")]
+    NoShutdownConfirmation,
+    #[error("NoPong error")]
+    NoPong,
+    #[error("ConnectionUpdate error")]
+    ConnectionUpdate(SendError<WebSocketApiRes>),
     #[error("WebSocket generic error: {0}")]
     Generic(String),
 }
