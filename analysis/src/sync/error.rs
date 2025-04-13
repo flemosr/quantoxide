dfe0cf800486040f6bba2f93ecc92e665b6fa62e
Rafael Lemos <contact@flemosr.com>
2025-04-13 13:53:16 -0300

    analysis: adjust `SyncError`

--- analysis/src/sync/error.rs
+++ analysis/src/sync/error.rs
@@ -1,9 +1,10 @@
-use std::result;
+use std::{result, sync::Arc};
 use thiserror::Error;
+use tokio::{sync::broadcast::error::SendError, task::JoinError};
 
 use super::{
     real_time_collection_task::error::RealTimeCollectionError,
-    sync_price_history_task::error::SyncPriceHistoryError,
+    sync_price_history_task::error::SyncPriceHistoryError, SyncState,
 };
 
 #[derive(Error, Debug)]
@@ -13,11 +14,19 @@ pub enum SyncError {
     #[error("RealTimeCollection error: {0}")]
     RealTimeCollection(#[from] RealTimeCollectionError),
     #[error("SyncTransmiter failed error {0}")]
-    SyncTransmiterFailed(String),
+    SyncTransmiterFailed(SendError<Arc<SyncState>>),
     #[error("TaskJoin error {0}")]
-    TaskJoin(String),
+    TaskJoin(JoinError),
     #[error("UnexpectedRealTimeCollectionShutdown error")]
     UnexpectedRealTimeCollectionShutdown,
 }
+
+impl PartialEq for SyncError {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string() == other.to_string()
+    }
+}
+
+impl Eq for SyncError {}
 
 pub type Result<T> = result::Result<T, SyncError>;
