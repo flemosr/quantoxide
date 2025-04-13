38608c56371b118ed97e6e98caed94aacc4a328f
Rafael Lemos <contact@flemosr.com>
2025-04-13 17:11:15 -0300

    analysis: adjust `SignalError`

--- analysis/src/signal/error.rs
+++ analysis/src/signal/error.rs
@@ -1,10 +1,25 @@
-use std::result;
+use std::{result, sync::Arc};
 use thiserror::Error;
+use tokio::{sync::broadcast::error::SendError, task::JoinError};
+
+use super::SignalJobState;
 
 #[derive(Error, Debug)]
 pub enum SignalError {
     #[error("Generic error, {0}")]
     Generic(String),
+    #[error("SignalTransmiterFailed failed error {0}")]
+    SignalTransmiterFailed(SendError<Arc<SignalJobState>>),
+    #[error("TaskJoin error {0}")]
+    TaskJoin(JoinError),
 }
+
+impl PartialEq for SignalError {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string() == other.to_string()
+    }
+}
+
+impl Eq for SignalError {}
 
 pub type Result<T> = result::Result<T, SignalError>;
