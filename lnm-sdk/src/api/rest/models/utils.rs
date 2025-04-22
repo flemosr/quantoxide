71887841224887045b9337a6d01e5a0e8302dc99
Rafael Lemos <contact@flemosr.com>
2025-04-22 14:08:36 -0300

    lnm-sdk: adjust `rest::models::utils`

--- lnm-sdk/src/api/rest/models/utils.rs
+++ lnm-sdk/src/api/rest/models/utils.rs
@@ -1,5 +1,5 @@
 pub mod float_without_decimal {
-    use serde::{self, Serializer};
+    use serde::Serializer;
 
     pub fn serialize<S>(value: &f64, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -9,6 +9,33 @@ pub mod float_without_decimal {
             serializer.serialize_i64(*value as i64)
         } else {
             serializer.serialize_f64(*value)
+        }
+    }
+}
+
+pub mod price_option {
+    use serde::{Deserialize, de};
+
+    use super::super::Price;
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Price>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let opt_price_f64 = Option::<f64>::deserialize(deserializer)?;
+
+        match opt_price_f64 {
+            None => Ok(None),
+            Some(price_f64) => {
+                if price_f64 == 0.0 {
+                    Ok(None)
+                } else {
+                    match Price::try_from(price_f64) {
+                        Ok(price) => Ok(Some(price)),
+                        Err(e) => Err(de::Error::custom(e.to_string())),
+                    }
+                }
+            }
         }
     }
 }
