dd3a3c9dd7cf43f3ef549c00ebd6975859c55134
Rafael Lemos <contact@flemosr.com>
2025-04-22 11:40:43 -0300

    lnm-sdk: reorganize `rest::models` mod

--- lnm-sdk/src/api/rest/models/mod.rs
+++ lnm-sdk/src/api/rest/models/mod.rs
@@ -1,10 +1,8 @@
-use chrono::{DateTime, Utc, serde::ts_milliseconds};
-use serde::Deserialize;
-
 mod error;
 mod leverage;
 mod margin;
 mod price;
+mod price_history;
 mod quantity;
 mod trade;
 mod utils;
@@ -12,22 +10,6 @@ mod utils;
 pub use leverage::Leverage;
 pub use margin::Margin;
 pub use price::Price;
+pub use price_history::PriceEntryLNM;
 pub use quantity::Quantity;
 pub use trade::{FuturesTradeRequestBody, Trade, TradeSide, TradeType};
-
-#[derive(Debug, Deserialize)]
-pub struct PriceEntryLNM {
-    #[serde(with = "ts_milliseconds")]
-    time: DateTime<Utc>,
-    value: f64,
-}
-
-impl PriceEntryLNM {
-    pub fn time(&self) -> &DateTime<Utc> {
-        &self.time
-    }
-
-    pub fn value(&self) -> f64 {
-        self.value
-    }
-}
