4dfba1575fd5fbffebda65638d106c3ba506548c
Rafael Lemos <contact@flemosr.com>
2025-04-23 09:36:40 -0300

    lnm-sdk: add `FuturesRepository::create_new_trade_quantity_limit`

--- lnm-sdk/src/api/rest/repositories.rs
+++ lnm-sdk/src/api/rest/repositories.rs
@@ -3,7 +3,7 @@ use chrono::{DateTime, Utc};
 
 use super::{
     error::Result,
-    models::{Leverage, Margin, Price, PriceEntryLNM, Trade, TradeSide},
+    models::{Leverage, Margin, Price, PriceEntryLNM, Quantity, Trade, TradeSide},
 };
 
 #[async_trait]
@@ -14,6 +14,16 @@ pub trait FuturesRepository: Send + Sync {
         to: Option<&DateTime<Utc>>,
         limit: Option<usize>,
     ) -> Result<Vec<PriceEntryLNM>>;
+
+    async fn create_new_trade_quantity_limit(
+        &self,
+        side: TradeSide,
+        quantity: Quantity,
+        leverage: Leverage,
+        price: Price,
+        stoploss: Option<Price>,
+        takeprofit: Option<Price>,
+    ) -> Result<Trade>;
 
     async fn create_new_trade_margin_limit(
         &self,
