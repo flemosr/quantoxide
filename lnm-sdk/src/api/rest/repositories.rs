f7af50c7039f2a1cf930059b192cd5f7652fc2ca
Rafael Lemos <contact@flemosr.com>
2025-04-25 19:48:14 -0300

    lnm-sdk: add `FuturesRepository::cancel_all_trades`

--- lnm-sdk/src/api/rest/repositories.rs
+++ lnm-sdk/src/api/rest/repositories.rs
@@ -39,6 +39,8 @@ pub trait FuturesRepository: Send + Sync {
 
     async fn cancel_trade(&self, id: Uuid) -> Result<Trade>;
 
+    async fn cancel_all_trades(&self) -> Result<Vec<Trade>>;
+
     async fn close_trade(&self, id: Uuid) -> Result<Trade>;
 
     async fn ticker(&self) -> Result<Ticker>;
