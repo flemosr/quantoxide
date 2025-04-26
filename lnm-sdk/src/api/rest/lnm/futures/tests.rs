a18e3e4af9578bc211f2a3f340d3be9c48dfa232
Rafael Lemos <contact@flemosr.com>
2025-04-25 21:14:44 -0300

    lnm-sdk: adjust `rest::lnm::futures::tests` mod
    Use single test to allow returned API values to be used in downstream assertions,
    add clean-up of test trades.

--- lnm-sdk/src/api/rest/lnm/futures/tests.rs
+++ lnm-sdk/src/api/rest/lnm/futures/tests.rs
@@ -1,6 +1,5 @@
 use dotenv::dotenv;
 use std::env;
-use tokio::time::{Duration, sleep};
 
 use super::super::super::models::{Margin, Quantity};
 use super::*;
@@ -21,51 +20,22 @@ fn init_repository_from_env() -> LnmFuturesRepository {
     LnmFuturesRepository::new(base)
 }
 
-async fn get_ask_price(repo: &LnmFuturesRepository) -> Price {
-    let ticker = repo.ticker().await.expect("must get ticker");
-    ticker.ask_price()
-}
-
-async fn get_out_of_market_price(repo: &LnmFuturesRepository) -> Price {
-    let ask = get_ask_price(repo).await;
-    ask.apply_change(-0.3).unwrap()
-}
-
-#[tokio::test]
-async fn test_cancel_all_trades() {
-    let repo = init_repository_from_env();
-
-    let trades = repo.cancel_all_trades().await.expect("must cancel trades");
-
-    assert!(trades.is_empty());
-}
-
-#[tokio::test]
-async fn test_close_all_trades() {
-    let repo = init_repository_from_env();
-
-    let trades = repo.close_all_trades().await.expect("must close trades");
-
-    assert!(trades.is_empty());
-}
-
-#[tokio::test]
-async fn test_ticker() {
-    let repo = init_repository_from_env();
-
+async fn test_ticker(repo: &LnmFuturesRepository) -> Ticker {
     let ticker = repo.ticker().await.expect("must get ticker");
 
     assert!(!ticker.exchanges_weights().is_empty());
+
+    ticker
 }
 
-#[tokio::test]
-async fn test_create_new_trade_quantity_limit() {
-    let repo = init_repository_from_env();
-
+async fn test_create_new_trade_quantity_limit(
+    repo: &LnmFuturesRepository,
+    ticker: &Ticker,
+) -> Trade {
     let side = TradeSide::Buy;
     let quantity = Quantity::try_from(1).unwrap();
     let leverage = Leverage::try_from(1).unwrap();
-    let out_of_mkt_price = get_out_of_market_price(&repo).await;
+    let out_of_mkt_price = ticker.ask_price().apply_change(-0.3).unwrap();
     let stoploss = Some(out_of_mkt_price.apply_change(-0.05).unwrap());
     let takeprofit = Some(out_of_mkt_price.apply_change(0.05).unwrap());
     let execution = out_of_mkt_price.into();
@@ -98,12 +68,11 @@ async fn test_create_new_trade_quantity_limit() {
     assert!(created_trade.market_filled_ts().is_none());
     assert!(created_trade.closed_ts().is_none());
     assert!(created_trade.exit_price().is_none());
+
+    created_trade
 }
 
-#[tokio::test]
-async fn test_create_new_trade_quantity_market() {
-    let repo = init_repository_from_env();
-
+async fn test_create_new_trade_quantity_market(repo: &LnmFuturesRepository) -> Trade {
     let side = TradeSide::Buy;
     let quantity = Quantity::try_from(1).unwrap();
     let leverage = Leverage::try_from(1).unwrap();
@@ -138,25 +107,19 @@ async fn test_create_new_trade_quantity_market() {
     assert!(created_trade.market_filled_ts().is_some());
     assert!(created_trade.closed_ts().is_none());
     assert!(created_trade.exit_price().is_none());
+
+    created_trade
 }
 
-#[tokio::test]
-async fn test_create_new_trade_margin_limit() {
-    let repo = init_repository_from_env();
-
+async fn test_create_new_trade_margin_limit(repo: &LnmFuturesRepository, ticker: &Ticker) -> Trade {
     let side = TradeSide::Buy;
     let leverage = Leverage::try_from(1).unwrap();
-    let out_of_mkt_price = get_out_of_market_price(&repo).await;
+    let out_of_mkt_price = ticker.ask_price().apply_change(-0.3).unwrap();
     let implied_qtd = Quantity::try_from(1).unwrap();
     let margin = Margin::try_calculate(implied_qtd, out_of_mkt_price, leverage).unwrap();
     let stoploss = Some(out_of_mkt_price.apply_change(-0.05).unwrap());
     let takeprofit = None;
     let execution = out_of_mkt_price.into();
-
-    println!(
-        "out_of_mkt_price {:?} margin {:?}",
-        out_of_mkt_price, margin
-    );
 
     let created_trade = repo
         .create_new_trade(
@@ -186,13 +149,15 @@ async fn test_create_new_trade_margin_limit() {
     assert!(created_trade.market_filled_ts().is_none());
     assert!(created_trade.closed_ts().is_none());
     assert!(created_trade.exit_price().is_none());
+
+    created_trade
 }
 
-#[tokio::test]
-async fn test_create_new_trade_margin_market() {
-    let repo = init_repository_from_env();
-
-    let est_min_price = get_ask_price(&repo).await.apply_change(-0.1).unwrap();
+async fn test_create_new_trade_margin_market(
+    repo: &LnmFuturesRepository,
+    ticker: &Ticker,
+) -> Trade {
+    let est_min_price = ticker.ask_price().apply_change(-0.1).unwrap();
 
     let side = TradeSide::Buy;
     let leverage = Leverage::try_from(1).unwrap();
@@ -229,93 +194,136 @@ async fn test_create_new_trade_margin_market() {
     assert!(created_trade.market_filled_ts().is_some());
     assert!(created_trade.closed_ts().is_none());
     assert!(created_trade.exit_price().is_none());
+
+    created_trade
 }
 
-#[tokio::test]
-async fn test_get_trades() {
-    let repo = init_repository_from_env();
-
-    let to = Utc::now();
-    let from = to - chrono::Duration::days(1);
-    let limit = Some(1);
-
-    let _ = repo
-        .get_trades(TradeStatus::Open, Some(&from), Some(&to), limit)
+async fn test_get_trades_open(repo: &LnmFuturesRepository, exp_open_trades: Vec<&Trade>) {
+    let open_trades = repo
+        .get_trades(TradeStatus::Open, None, None, None)
         .await
         .expect("must get trades");
+
+    assert_eq!(open_trades.len(), exp_open_trades.len());
+
+    for open_trade in &open_trades {
+        let ok = exp_open_trades
+            .iter()
+            .any(|exp| exp.id() == open_trade.id());
+        assert!(ok, "open trade {} was not returned", open_trade.id());
+    }
 }
 
-#[tokio::test]
-async fn test_cancel_trade() {
-    let repo = init_repository_from_env();
+async fn test_get_trades_running(repo: &LnmFuturesRepository, exp_running_trades: Vec<&Trade>) {
+    let running_trades = repo
+        .get_trades(TradeStatus::Running, None, None, None)
+        .await
+        .expect("must get trades");
 
-    let price = get_out_of_market_price(&repo).await;
-    let stoploss = Some(price.apply_change(-0.05).unwrap());
-    let takeprofit = Some(price.apply_change(0.05).unwrap());
-    let execution = price.into();
+    assert_eq!(running_trades.len(), exp_running_trades.len());
 
-    let created_trade = repo
-        .create_new_trade(
-            TradeSide::Buy,
-            Quantity::try_from(1).unwrap().into(),
-            Leverage::try_from(1).unwrap(),
-            execution,
-            stoploss,
-            takeprofit,
+    for running_trade in &running_trades {
+        let ok = exp_running_trades
+            .iter()
+            .any(|exp| exp.id() == running_trade.id());
+        assert!(ok, "running trade {} was not returned", running_trade.id());
+    }
+}
+
+async fn test_get_trades_closed(repo: &LnmFuturesRepository, exp_closed_trades: Vec<&Trade>) {
+    let closed_trades = repo
+        .get_trades(
+            TradeStatus::Closed,
+            None,
+            None,
+            Some(exp_closed_trades.len()),
         )
         .await
-        .expect("must create trade");
+        .expect("must get trades");
 
-    assert!(created_trade.open());
-    assert!(!created_trade.running());
-    assert!(!created_trade.closed());
-    assert!(!created_trade.canceled());
+    assert_eq!(closed_trades.len(), exp_closed_trades.len());
 
-    sleep(Duration::from_secs(1)).await;
+    for closed_trade in &closed_trades {
+        let ok = exp_closed_trades
+            .iter()
+            .any(|exp| exp.id() == closed_trade.id());
+        assert!(ok, "closed trade {} was not returned", closed_trade.id());
+    }
+}
 
-    let canceled_trade = repo
-        .cancel_trade(created_trade.id())
-        .await
-        .expect("must cancel trade");
+async fn test_cancel_trade(repo: &LnmFuturesRepository, id: Uuid) {
+    let canceled_trade = repo.cancel_trade(id).await.expect("must cancel trade");
 
-    assert_eq!(canceled_trade.id(), created_trade.id());
+    assert_eq!(canceled_trade.id(), id);
     assert!(!canceled_trade.open());
     assert!(!canceled_trade.running());
     assert!(!canceled_trade.closed());
     assert!(canceled_trade.canceled());
 }
 
-#[tokio::test]
-async fn test_close_trade() {
-    let repo = init_repository_from_env();
+async fn test_close_trade(repo: &LnmFuturesRepository, id: Uuid) {
+    let closed_trade = repo.close_trade(id).await.expect("must close trade");
 
-    let created_trade = repo
-        .create_new_trade(
-            TradeSide::Buy,
-            Quantity::try_from(1).unwrap().into(),
-            Leverage::try_from(1).unwrap(),
-            TradeExecution::Market,
-            None,
-            None,
-        )
-        .await
-        .expect("must create trade");
-
-    assert!(!created_trade.open());
-    assert!(created_trade.running());
-    assert!(!created_trade.closed());
-    assert!(!created_trade.canceled());
-
-    sleep(Duration::from_secs(1)).await;
-
-    let closed_trade = repo
-        .close_trade(created_trade.id())
-        .await
-        .expect("must close trade");
-
-    assert_eq!(closed_trade.id(), created_trade.id());
+    assert_eq!(closed_trade.id(), id);
     assert!(!closed_trade.open());
     assert!(!closed_trade.running());
     assert!(closed_trade.closed());
     assert!(!closed_trade.canceled());
+}
+
+async fn test_cancel_all_trades(repo: &LnmFuturesRepository, exp_open_trades: Vec<&Trade>) {
+    let cancelled_trades = repo.cancel_all_trades().await.expect("must cancel trades");
+
+    assert_eq!(cancelled_trades.len(), exp_open_trades.len());
+
+    for open_trade in &exp_open_trades {
+        let cancelled = cancelled_trades
+            .iter()
+            .any(|cancelled| cancelled.id() == open_trade.id());
+        assert!(cancelled, "trade {} was not cancelled", open_trade.id());
+    }
+}
+
+async fn test_close_all_trades(repo: &LnmFuturesRepository, exp_running_trades: Vec<&Trade>) {
+    let closed_trades = repo.close_all_trades().await.expect("must close trades");
+
+    assert_eq!(closed_trades.len(), exp_running_trades.len());
+
+    for running_trade in &exp_running_trades {
+        let closed = closed_trades
+            .iter()
+            .any(|closed| closed.id() == running_trade.id());
+        assert!(closed, "trade {} was not closed", running_trade.id());
+    }
+}
+
+#[tokio::test]
+async fn test_api() {
+    let repo = init_repository_from_env();
+
+    // Initial clean-up
+    repo.cancel_all_trades().await.expect("must cancel trades");
+    repo.close_all_trades().await.expect("must close trades");
+
+    let ticker = test_ticker(&repo).await;
+
+    let limit_trade_a = test_create_new_trade_quantity_limit(&repo, &ticker).await;
+    let limit_trade_b = test_create_new_trade_margin_limit(&repo, &ticker).await;
+
+    test_get_trades_open(&repo, vec![&limit_trade_a, &limit_trade_b]).await;
+
+    let market_trade_a = test_create_new_trade_quantity_market(&repo).await;
+    let market_trade_b = test_create_new_trade_margin_market(&repo, &ticker).await;
+
+    test_get_trades_running(&repo, vec![&market_trade_a, &market_trade_b]).await;
+
+    test_close_trade(&repo, market_trade_a.id()).await;
+
+    test_close_all_trades(&repo, vec![&market_trade_b]).await;
+
+    test_cancel_trade(&repo, limit_trade_a.id()).await;
+
+    test_cancel_all_trades(&repo, vec![&limit_trade_b]).await;
+
+    test_get_trades_closed(&repo, vec![&market_trade_a, &market_trade_b]).await;
 }
