2a22112de610aae9e1883b8017484976d68b6e16
Rafael Lemos <contact@flemosr.com>
2025-04-29 08:26:41 -0300

    quantoxide: adjust `trade` mod, use `lnm-sdk` types

--- quantoxide/src/trade/mod.rs
+++ quantoxide/src/trade/mod.rs
@@ -1,97 +1,12 @@
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 
-use lnm_sdk::api::rest::models::{Leverage, Margin};
+use lnm_sdk::api::rest::models::{BoundedPercentage, Leverage, LowerBoundedPercentage, Margin};
 
 mod error;
 mod simulated;
 
-use error::{Result, TradeError};
-
-/// Represents a percentage value that is constrained within a specific range.
-///
-/// This struct wraps an f32 value that must be:
-/// - Greater than or equal to 0.1%
-/// - Less than or equal to 99.9%
-///
-/// This bounded range makes it suitable for percentage calculations where both
-/// minimum and maximum limits are required.
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct BoundedPercentage(f32);
-
-impl TryFrom<f32> for BoundedPercentage {
-    type Error = TradeError;
-
-    fn try_from(value: f32) -> Result<Self> {
-        if value < 0.1 || value > 99.9 {
-            return Err(TradeError::Generic(format!(
-                "`BoundedPercentage` must be gte 0.1 and lte 99.9, got {value}"
-            )));
-        }
-        Ok(Self(value))
-    }
-}
-
-impl From<BoundedPercentage> for f32 {
-    fn from(perc: BoundedPercentage) -> f32 {
-        perc.0
-    }
-}
-
-impl Eq for BoundedPercentage {}
-
-impl Ord for BoundedPercentage {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other)
-            .expect("`BoundedPercentage` must be finite")
-    }
-}
-
-/// Represents a percentage value that is only constrained by a lower bound.
-///
-/// This struct wraps an f32 value that must be:
-/// - Greater than or equal to 0.1%
-/// - Finite (not infinity)
-///
-/// This type is suitable for percentage calculations where only a minimum
-/// threshold is needed, with no practical upper limit other than it must be a
-/// finite value.
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct LowerBoundedPercentage(f32);
-
-impl TryFrom<f32> for LowerBoundedPercentage {
-    type Error = TradeError;
-
-    fn try_from(value: f32) -> Result<Self> {
-        if value < 0.1 {
-            return Err(TradeError::Generic(format!(
-                "`LowerBoundedPercentage` must be gte 0.1, got {value}"
-            )));
-        }
-        if value == f32::INFINITY {
-            return Err(TradeError::Generic(format!(
-                "`LowerBoundedPercentage` must be finite"
-            )));
-        }
-
-        Ok(Self(value))
-    }
-}
-
-impl From<LowerBoundedPercentage> for f32 {
-    fn from(perc: LowerBoundedPercentage) -> f32 {
-        perc.0
-    }
-}
-
-impl Eq for LowerBoundedPercentage {}
-
-impl Ord for LowerBoundedPercentage {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other)
-            .expect("`LowerBoundedPercentage` must be finite")
-    }
-}
+use error::Result;
 
 pub enum TradeOrder {
     OpenLong {
