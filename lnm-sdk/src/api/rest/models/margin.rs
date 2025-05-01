0d590129df16562956d59f6bbd64208e6bbc0216
Rafael Lemos <contact@flemosr.com>
2025-05-01 15:26:22 -0300

    lnm-sdk: adjust `models::Margin`, add basic tests

--- lnm-sdk/src/api/rest/models/margin.rs
+++ lnm-sdk/src/api/rest/models/margin.rs
@@ -17,8 +17,8 @@ impl Margin {
         leverage: Leverage,
     ) -> Result<Self, MarginValidationError> {
         let margin =
-            quantity.into_u64() as f64 / (price.into_f64() * leverage.into_f64()) * 100000000.;
-        Self::try_from(margin.ceil() as u64)
+            quantity.into_u64() as f64 * (100_000_000. / (price.into_f64() * leverage.into_f64()));
+        Self::try_from(margin.floor() as u64)
     }
 }
 
@@ -80,90 +80,6 @@ impl TryFrom<f64> for Margin {
     }
 }
 
-// impl PartialEq for Margin {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.0 == other.0
-//     }
-// }
-
-// impl Eq for Margin {}
-
-// impl PartialOrd for Margin {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         Some(self.cmp(other))
-//     }
-// }
-
-// impl Ord for Margin {
-//     fn cmp(&self, other: &Self) -> Ordering {
-//         self.0.cmp(&other.0)
-//     }
-// }
-
-// impl PartialEq<u64> for Margin {
-//     fn eq(&self, other: &u64) -> bool {
-//         self.0 == *other
-//     }
-// }
-
-// impl PartialOrd<u64> for Margin {
-//     fn partial_cmp(&self, other: &u64) -> Option<Ordering> {
-//         Some(self.0.cmp(other))
-//     }
-// }
-
-// impl PartialEq<Margin> for u64 {
-//     fn eq(&self, other: &Margin) -> bool {
-//         *self == other.0
-//     }
-// }
-
-// impl PartialOrd<Margin> for u64 {
-//     fn partial_cmp(&self, other: &Margin) -> Option<Ordering> {
-//         Some(self.cmp(&other.0))
-//     }
-// }
-
-// impl PartialEq<i32> for Margin {
-//     fn eq(&self, other: &i32) -> bool {
-//         if *other < 0 {
-//             false
-//         } else {
-//             self.0 == *other as u64
-//         }
-//     }
-// }
-
-// impl PartialOrd<i32> for Margin {
-//     fn partial_cmp(&self, other: &i32) -> Option<Ordering> {
-//         if *other < 0 {
-//             Some(Ordering::Greater)
-//         } else {
-//             Some(self.0.cmp(&(*other as u64)))
-//         }
-//     }
-// }
-
-// impl PartialEq<Margin> for i32 {
-//     fn eq(&self, other: &Margin) -> bool {
-//         if *self < 0 {
-//             false
-//         } else {
-//             *self as u64 == other.0
-//         }
-//     }
-// }
-
-// impl PartialOrd<Margin> for i32 {
-//     fn partial_cmp(&self, other: &Margin) -> Option<Ordering> {
-//         if *self < 0 {
-//             Some(Ordering::Less)
-//         } else {
-//             Some((*self as u64).cmp(&other.0))
-//         }
-//     }
-// }
-
 impl Serialize for Margin {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -180,5 +96,32 @@ impl<'de> Deserialize<'de> for Margin {
     {
         let margin_u64 = u64::deserialize(deserializer)?;
         Margin::try_from(margin_u64).map_err(|e| de::Error::custom(e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_margin() {
+        let quantity = Quantity::try_from(5).unwrap();
+        let price = Price::try_from(95000).unwrap();
+        let leverage = Leverage::try_from(1.0).unwrap();
+
+        let margin = Margin::try_calculate(quantity, price, leverage).unwrap();
+        assert_eq!(margin.into_u64(), 5263);
+
+        let leverage = Leverage::try_from(2.0).unwrap();
+        let margin = Margin::try_calculate(quantity, price, leverage).unwrap();
+        assert_eq!(margin.into_u64(), 2631);
+
+        let leverage = Leverage::try_from(50.0).unwrap();
+        let margin = Margin::try_calculate(quantity, price, leverage).unwrap();
+        assert_eq!(margin.into_u64(), 105);
+
+        let leverage = Leverage::try_from(100.0).unwrap();
+        let margin = Margin::try_calculate(quantity, price, leverage).unwrap();
+        assert_eq!(margin.into_u64(), 52);
     }
 }
