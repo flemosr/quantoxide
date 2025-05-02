1934ed48d8cc10993228f0e70e72171f29d45db2
Rafael Lemos <contact@flemosr.com>
2025-05-02 08:36:39 -0300

    quantoxide: adjust `TradeError`

--- quantoxide/src/trade/error.rs
+++ quantoxide/src/trade/error.rs
@@ -1,8 +1,12 @@
 use std::result;
 use thiserror::Error;
 
+use super::simulation::error::SimulationError;
+
 #[derive(Error, Debug)]
 pub enum TradeError {
+    #[error("[Simulated] {0}")]
+    Simulated(#[from] SimulationError),
     #[error("Generic error, {0}")]
     Generic(String),
 }
