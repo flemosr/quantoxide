e70b90e44137d871af31d087f52b7b922ab402c8
Rafael Lemos <contact@flemosr.com>
2025-05-13 20:41:49 -0300

    quantoxide: adjust `LiveError`

--- quantoxide/src/trade/live/error.rs
+++ quantoxide/src/trade/live/error.rs
@@ -1,11 +1,14 @@
 use thiserror::Error;
 
-use lnm_sdk::api::rest::models::error::QuantityValidationError;
+use lnm_sdk::api::rest::{error::RestApiError, models::error::QuantityValidationError};
 
 #[derive(Error, Debug)]
 pub enum LiveError {
     #[error("[QuantityValidation] {0}")]
     QuantityValidation(#[from] QuantityValidationError),
+
+    #[error("[RestApi] {0}")]
+    RestApi(#[from] RestApiError),
 
     #[error("Generic error, {0}")]
     Generic(String),
