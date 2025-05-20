8c1e621a3a69e3ec19a1cb583ed3f72b8a9979d8
Rafael Lemos <contact@flemosr.com>
2025-05-20 11:49:42 -0300

    quantoxide: add `util::ShutdownForwarder`

--- quantoxide/src/util.rs
+++ quantoxide/src/util.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Duration, SubsecRound, Utc};
+use tokio::{sync::broadcast, task::JoinHandle};
 
 /// A type that can not be instantiated
 pub enum Never {}
@@ -21,5 +22,34 @@ impl DateTimeExt for DateTime<Utc> {
 
     fn is_round(&self) -> bool {
         *self == self.trunc_subsecs(0)
+    }
+}
+
+pub struct ShutdownForwarder(JoinHandle<()>);
+
+impl ShutdownForwarder {
+    /// Creates a new forwarder that pipes signals from `external_rx` to
+    /// `internal_tx`.
+    ///
+    /// **Important**: An `internal_tx` signal is sent if an `external_rx` is
+    /// received or if the `external_rx` channel is closed.
+    pub fn new(
+        mut external_rx: broadcast::Receiver<()>,
+        internal_tx: broadcast::Sender<()>,
+    ) -> Self {
+        let handle = tokio::spawn(async move {
+            let _ = external_rx.recv().await;
+
+            // Send signal if a shutdown is received, or if `external_rx` is closed
+            let _ = internal_tx.send(());
+        });
+
+        Self(handle)
+    }
+}
+
+impl Drop for ShutdownForwarder {
+    fn drop(&mut self) {
+        self.0.abort();
     }
 }
