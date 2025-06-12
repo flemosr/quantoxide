4a19dd5a6fe16d51ec0b3a0be560bd24164fde93
Rafael Lemos <contact@flemosr.com>
2025-06-12 15:40:25 -0300

    quantoxide: remove `PriceHistoryEntryLOCF`'s `fmt::Display` impl

--- quantoxide/src/db/models.rs
+++ quantoxide/src/db/models.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 use chrono::{DateTime, Utc};
 use sqlx::FromRow;
 
@@ -18,34 +16,6 @@ pub struct PriceHistoryEntryLOCF {
     pub ma_5: Option<f64>,
     pub ma_60: Option<f64>,
     pub ma_300: Option<f64>,
-}
-
-impl fmt::Display for PriceHistoryEntryLOCF {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "price_history_entry_locf:")?;
-        writeln!(f, "  time: {}", self.time)?;
-        writeln!(f, "  value: {:.2}", self.value)?;
-
-        if let Some(ma_5) = self.ma_5 {
-            writeln!(f, "  ma_5: {:.2}", ma_5)?;
-        } else {
-            writeln!(f, "  ma_5: null")?;
-        }
-
-        if let Some(ma_60) = self.ma_60 {
-            writeln!(f, "  ma_60: {:.2}", ma_60)?;
-        } else {
-            writeln!(f, "  ma_60: null")?;
-        }
-
-        if let Some(ma_300) = self.ma_300 {
-            writeln!(f, "  ma_300: {:.2}", ma_300)?;
-        } else {
-            writeln!(f, "  ma_300: null")?;
-        }
-
-        Ok(())
-    }
 }
 
 #[derive(Debug, FromRow, Clone)]
