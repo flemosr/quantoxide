52d9e0d709fd4dfdb438a771ee8fa0c9f4ae6119
Rafael Lemos <contact@flemosr.com>
2025-06-13 10:36:29 -0300

    quantoxide: adjust `PriceHistoryState`
    Make `reach` optional, consolidate `next_download_range_` methods.

--- quantoxide/src/sync/sync_price_history_task/price_history_state.rs
+++ quantoxide/src/sync/sync_price_history_task/price_history_state.rs
@@ -8,14 +8,14 @@ use super::error::{Result, SyncPriceHistoryError};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PriceHistoryState {
-    reach_time: DateTime<Utc>,
+    reach_time_opt: Option<DateTime<Utc>>,
     bounds: Option<(DateTime<Utc>, DateTime<Utc>)>,
     entry_gaps: Vec<(DateTime<Utc>, DateTime<Utc>)>,
 }
 
 impl PriceHistoryState {
-    pub async fn evaluate(db: &DbContext, reach: Duration) -> Result<Self> {
-        let reach_time = Utc::now() - reach;
+    pub async fn evaluate(db: &DbContext, reach_opt: Option<Duration>) -> Result<Self> {
+        let reach_time_opt = reach_opt.map_or(None, |reach| Some(Utc::now() - reach));
 
         let earliest_entry = match db.price_history.get_earliest_entry().await? {
             Some(entry) => entry,
@@ -23,7 +23,7 @@ impl PriceHistoryState {
                 // DB is empty
 
                 return Ok(Self {
-                    reach_time,
+                    reach_time_opt,
                     bounds: None,
                     entry_gaps: Vec::new(),
                 });
@@ -39,15 +39,15 @@ impl PriceHistoryState {
         if earliest_entry.time == lastest_entry.time {
             // DB has a single entry
 
-            if earliest_entry.time < reach_time {
+            if reach_time_opt.map_or(false, |reach_time| earliest_entry.time < reach_time) {
                 return Err(SyncPriceHistoryError::UnreachableDbGap {
                     gap: earliest_entry.time,
-                    reach: reach_time,
+                    reach: reach_time_opt.expect("`reach_time_opt` can't be `None`"),
                 });
             }
 
             return Ok(Self {
-                reach_time,
+                reach_time_opt,
                 bounds: Some((earliest_entry.time, earliest_entry.time)),
                 entry_gaps: Vec::new(),
             });
@@ -56,37 +56,74 @@ impl PriceHistoryState {
         let entry_gaps = db.price_history.get_gaps().await?;
 
         if let Some((from_time, _)) = entry_gaps.first() {
-            if *from_time < reach_time {
-                // There is a price gap before `reach`. Since we shouldn't fetch entries
-                // before `reach`. Said gap can't be closed, and therefore the DB can't
-                // be synced.
+            if reach_time_opt.map_or(false, |reach_time| *from_time < reach_time) {
+                // There is a price gap before `reach_time`. Since entries before `reach_time`
+                // can't be fetched, said gap can't be closed.
+                // Therefore the DB can't be synced.
                 return Err(SyncPriceHistoryError::UnreachableDbGap {
                     gap: *from_time,
-                    reach: reach_time,
+                    reach: reach_time_opt.expect("`reach_time_opt` can't be `None`"),
                 });
             }
         }
 
         Ok(Self {
-            reach_time,
+            reach_time_opt,
             bounds: Some((earliest_entry.time, lastest_entry.time)),
             entry_gaps,
         })
     }
 
-    pub fn next_download_range_backfill(&self) -> (Option<&DateTime<Utc>>, Option<&DateTime<Utc>>) {
-        let history_bounds = match &self.bounds {
-            Some(bounds) => bounds,
-            None => return (None, None),
+    pub fn next_download_range(
+        &self,
+        backfilling: bool,
+    ) -> Result<(Option<DateTime<Utc>>, Option<DateTime<Utc>>)> {
+        let Some(reach_time) = self.reach_time_opt else {
+            return Err(SyncPriceHistoryError::Generic(
+                "`reach` was not set".to_string(),
+            ));
         };
 
-        if let Some((gap_from, gap_to)) = self.entry_gaps.first() {
-            return (Some(gap_from), Some(gap_to));
+        let history_bounds = match &self.bounds {
+            Some(bounds) => bounds,
+            None => return Ok((None, None)),
+        };
+
+        if history_bounds.0 == history_bounds.1 && history_bounds.0 < reach_time {
+            // DB has a single unreachable entry. Edge case
+
+            return Err(SyncPriceHistoryError::UnreachableDbGap {
+                gap: history_bounds.0,
+                reach: reach_time,
+            });
         }
-        if history_bounds.0 > self.reach_time {
-            return (None, Some(&history_bounds.0));
+
+        let prioritized_gap = if backfilling {
+            self.entry_gaps.first()
+        } else {
+            self.entry_gaps.last()
+        };
+
+        if let Some((gap_from, gap_to)) = prioritized_gap {
+            if *gap_from < reach_time {
+                // Gap before `reach`. Since entries before `reach` can't be fetched, said gap
+                // can't be closed. Therefore, the DB can't be synced.
+                return Err(SyncPriceHistoryError::UnreachableDbGap {
+                    gap: *gap_from,
+                    reach: reach_time,
+                });
+            }
+
+            return Ok((Some(*gap_from), Some(*gap_to)));
         }
-        (Some(&history_bounds.1), None)
+
+        if backfilling && history_bounds.0 > reach_time {
+            // Price history can be extended further into the past
+
+            return Ok((None, Some(history_bounds.0)));
+        }
+
+        Ok((Some(history_bounds.1), None))
     }
 
     pub fn get_upper_history_bound(&self) -> Option<DateTime<Utc>> {
@@ -103,25 +140,16 @@ impl PriceHistoryState {
         Some(history_bounds.1 - history_bounds.0)
     }
 
-    pub fn next_download_range_live(&self) -> (Option<&DateTime<Utc>>, Option<&DateTime<Utc>>) {
-        let history_bounds = match &self.bounds {
-            Some(bounds) => bounds,
-            None => return (None, None),
+    pub fn has_gaps(&self) -> Result<bool> {
+        let Some(reach_time) = self.reach_time_opt else {
+            return Err(SyncPriceHistoryError::Generic(
+                "`reach` was not set".to_string(),
+            ));
         };
 
-        if let Some((gap_from, gap_to)) = self.entry_gaps.last() {
-            return (Some(gap_from), Some(gap_to));
-        }
-        if history_bounds.0 > self.reach_time {
-            return (None, Some(&history_bounds.0));
-        }
-        (Some(&history_bounds.1), None)
-    }
-
-    pub fn has_gaps(&self) -> bool {
-        self.bounds.is_none()
-            || self.reach_time < self.bounds.expect("not none").0
-            || !self.entry_gaps.is_empty()
+        Ok(self.bounds.map_or(true, |bounds| {
+            !self.entry_gaps.is_empty() || reach_time < bounds.0
+        }))
     }
 }
 
@@ -137,15 +165,22 @@ fn eval_missing_hours(current: &DateTime<Utc>, target: &DateTime<Utc>) -> String
 impl fmt::Display for PriceHistoryState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "PriceHistoryState:")?;
-        writeln!(f, "  reach: {}", self.reach_time.to_rfc3339())?;
+        if let Some(reach_time) = self.reach_time_opt {
+            writeln!(f, "  reach: {}", reach_time.to_rfc3339())?;
+        }
 
         match &self.bounds {
             Some((start, end)) => {
-                let start_eval = eval_missing_hours(start, &self.reach_time);
-                let end_val = eval_missing_hours(&Utc::now(), end);
-
                 writeln!(f, "  bounds:")?;
-                writeln!(f, "    start: {} ({start_eval})", start.to_rfc3339())?;
+
+                if let Some(reach_time) = self.reach_time_opt {
+                    let start_eval = eval_missing_hours(start, &reach_time);
+                    writeln!(f, "    start: {} ({start_eval})", start.to_rfc3339())?;
+                } else {
+                    writeln!(f, "    start: {}", start.to_rfc3339())?;
+                };
+
+                let end_val = eval_missing_hours(&Utc::now(), end);
                 writeln!(f, "    end: {} ({end_val})", end.to_rfc3339())?;
 
                 if self.entry_gaps.is_empty() {
