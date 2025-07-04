df8d0c1f7a042eb1ebd753e635c20fca68050580
Rafael Lemos <contact@flemosr.com>
2025-07-04 19:33:58 -0300

    quantoxide: reorganize `sync` mod, WIP

--- quantoxide/src/sync/mod.rs
+++ quantoxide/src/sync/mod.rs
@@ -1,6 +1,6 @@
 use std::{
     pin::Pin,
-    sync::{Arc, Mutex, MutexGuard},
+    sync::{Arc, Mutex},
 };
 
 use chrono::Duration;
@@ -19,126 +19,16 @@ use crate::{
 
 pub mod error;
 mod real_time_collection_task;
+mod state;
 mod sync_price_history_task;
 
 use error::{Result, SyncError};
 use real_time_collection_task::RealTimeCollectionTask;
-pub use sync_price_history_task::PriceHistoryState;
+use state::{SyncStateManager, SyncTransmiter};
 use sync_price_history_task::{PriceHistoryStateTransmiter, SyncPriceHistoryTask};
 
-#[derive(Debug, PartialEq)]
-pub enum SyncStateNotSynced {
-    NotInitiated,
-    Starting,
-    InProgress(PriceHistoryState),
-    WaitingForResync,
-    Failed(SyncError),
-    Restarting,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum SyncState {
-    NotSynced(Arc<SyncStateNotSynced>),
-    Synced,
-    ShutdownInitiated,
-    Shutdown,
-}
-
-impl From<SyncStateNotSynced> for SyncState {
-    fn from(value: SyncStateNotSynced) -> Self {
-        Self::NotSynced(Arc::new(value))
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum SyncUpdate {
-    StateChange(SyncState),
-    PriceTick(PriceTick),
-}
-
-impl From<SyncState> for SyncUpdate {
-    fn from(value: SyncState) -> Self {
-        Self::StateChange(value)
-    }
-}
-
-impl From<PriceTick> for SyncUpdate {
-    fn from(value: PriceTick) -> Self {
-        Self::PriceTick(value)
-    }
-}
-
-pub type SyncTransmiter = broadcast::Sender<SyncUpdate>;
-pub type SyncReceiver = broadcast::Receiver<SyncUpdate>;
-
-pub trait SyncReader: Send + Sync + 'static {
-    fn update_receiver(&self) -> SyncReceiver;
-    fn state_snapshot(&self) -> SyncState;
-}
-
-#[derive(Debug)]
-struct SyncStateManager {
-    state: Mutex<SyncState>,
-    update_tx: SyncTransmiter,
-}
-
-impl SyncStateManager {
-    pub fn new(update_tx: SyncTransmiter) -> Arc<Self> {
-        let state = Mutex::new(SyncStateNotSynced::NotInitiated.into());
-
-        Arc::new(Self { state, update_tx })
-    }
-
-    fn update_state_guard(&self, mut state_guard: MutexGuard<'_, SyncState>, new_state: SyncState) {
-        *state_guard = new_state.clone();
-        drop(state_guard);
-
-        // Ignore no-receivers errors
-        let _ = self.update_tx.send(new_state.into());
-    }
-
-    pub fn update(&self, new_state: SyncState) {
-        let state_guard = self
-            .state
-            .lock()
-            .expect("`SyncStateManager` mutex can't be poisoned");
-
-        self.update_state_guard(state_guard, new_state);
-    }
-
-    pub fn handle_price_history_state_update(&self, new_history_state: PriceHistoryState) {
-        let state_guard = self
-            .state
-            .lock()
-            .expect("`SyncStateManager` mutex can't be poisoned");
-
-        let SyncState::NotSynced(sync_state_not_synced) = &*state_guard else {
-            return;
-        };
-
-        if let SyncStateNotSynced::Starting
-        | SyncStateNotSynced::InProgress(_)
-        | SyncStateNotSynced::WaitingForResync = sync_state_not_synced.as_ref()
-        {
-            let new_state: SyncState = SyncStateNotSynced::InProgress(new_history_state).into();
-
-            self.update_state_guard(state_guard, new_state);
-        }
-    }
-}
-
-impl SyncReader for SyncStateManager {
-    fn update_receiver(&self) -> SyncReceiver {
-        self.update_tx.subscribe()
-    }
-
-    fn state_snapshot(&self) -> SyncState {
-        self.state
-            .lock()
-            .expect("`SyncStateManager` mutex can't be poisoned")
-            .clone()
-    }
-}
+pub use state::{SyncReader, SyncReceiver, SyncState, SyncStateNotSynced, SyncUpdate};
+pub use sync_price_history_task::PriceHistoryState;
 
 #[derive(Clone)]
 struct SyncProcessConfig {
---
+++ quantoxide/src/sync/state.rs
@@ -0,0 +1,137 @@
+use std::{
+    pin::Pin,
+    sync::{Arc, Mutex, MutexGuard},
+};
+
+use chrono::Duration;
+use tokio::{
+    sync::{broadcast, mpsc},
+    time,
+};
+
+use lnm_sdk::api::ApiContext;
+
+use crate::{
+    db::{DbContext, models::PriceTick},
+    trade::live::LiveConfig,
+    util::{AbortOnDropHandle, Never},
+};
+
+use super::error::{Result, SyncError};
+use super::real_time_collection_task::RealTimeCollectionTask;
+pub use super::sync_price_history_task::PriceHistoryState;
+use super::sync_price_history_task::{PriceHistoryStateTransmiter, SyncPriceHistoryTask};
+
+#[derive(Debug, PartialEq)]
+pub enum SyncStateNotSynced {
+    NotInitiated,
+    Starting,
+    InProgress(PriceHistoryState),
+    WaitingForResync,
+    Failed(SyncError),
+    Restarting,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum SyncState {
+    NotSynced(Arc<SyncStateNotSynced>),
+    Synced,
+    ShutdownInitiated,
+    Shutdown,
+}
+
+impl From<SyncStateNotSynced> for SyncState {
+    fn from(value: SyncStateNotSynced) -> Self {
+        Self::NotSynced(Arc::new(value))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum SyncUpdate {
+    StateChange(SyncState),
+    PriceTick(PriceTick),
+}
+
+impl From<SyncState> for SyncUpdate {
+    fn from(value: SyncState) -> Self {
+        Self::StateChange(value)
+    }
+}
+
+impl From<PriceTick> for SyncUpdate {
+    fn from(value: PriceTick) -> Self {
+        Self::PriceTick(value)
+    }
+}
+
+pub type SyncTransmiter = broadcast::Sender<SyncUpdate>;
+pub type SyncReceiver = broadcast::Receiver<SyncUpdate>;
+
+pub trait SyncReader: Send + Sync + 'static {
+    fn update_receiver(&self) -> SyncReceiver;
+    fn state_snapshot(&self) -> SyncState;
+}
+
+#[derive(Debug)]
+pub struct SyncStateManager {
+    state: Mutex<SyncState>,
+    update_tx: SyncTransmiter,
+}
+
+impl SyncStateManager {
+    pub fn new(update_tx: SyncTransmiter) -> Arc<Self> {
+        let state = Mutex::new(SyncStateNotSynced::NotInitiated.into());
+
+        Arc::new(Self { state, update_tx })
+    }
+
+    fn update_state_guard(&self, mut state_guard: MutexGuard<'_, SyncState>, new_state: SyncState) {
+        *state_guard = new_state.clone();
+        drop(state_guard);
+
+        // Ignore no-receivers errors
+        let _ = self.update_tx.send(new_state.into());
+    }
+
+    pub fn update(&self, new_state: SyncState) {
+        let state_guard = self
+            .state
+            .lock()
+            .expect("`SyncStateManager` mutex can't be poisoned");
+
+        self.update_state_guard(state_guard, new_state);
+    }
+
+    pub fn handle_price_history_state_update(&self, new_history_state: PriceHistoryState) {
+        let state_guard = self
+            .state
+            .lock()
+            .expect("`SyncStateManager` mutex can't be poisoned");
+
+        let SyncState::NotSynced(sync_state_not_synced) = &*state_guard else {
+            return;
+        };
+
+        if let SyncStateNotSynced::Starting
+        | SyncStateNotSynced::InProgress(_)
+        | SyncStateNotSynced::WaitingForResync = sync_state_not_synced.as_ref()
+        {
+            let new_state: SyncState = SyncStateNotSynced::InProgress(new_history_state).into();
+
+            self.update_state_guard(state_guard, new_state);
+        }
+    }
+}
+
+impl SyncReader for SyncStateManager {
+    fn update_receiver(&self) -> SyncReceiver {
+        self.update_tx.subscribe()
+    }
+
+    fn state_snapshot(&self) -> SyncState {
+        self.state
+            .lock()
+            .expect("`SyncStateManager` mutex can't be poisoned")
+            .clone()
+    }
+}
