b69cffb027dbf90271dff5cc2ce3498431b79434
Rafael Lemos <contact@flemosr.com>
2025-07-15 13:28:41 -0300

    quantoxide: adjust `LiveTuiView`
    Impl 'trades' and 'summary' panes.

--- quantoxide/src/trade/live_tui/view.rs
+++ quantoxide/src/trade/live_tui/view.rs
@@ -20,7 +20,8 @@ pub trait LiveTuiLogger: Sync + Send + 'static {
 
 #[derive(Debug, PartialEq)]
 enum ActivePane {
-    StatePane,
+    TradesPane,
+    SummaryPane,
     LogPane,
 }
 
@@ -34,11 +35,17 @@ struct LiveTuiViewState {
     log_v_scroll: usize,
     log_h_scroll: usize,
 
-    state_lines: Vec<String>,
-    state_max_line_width: usize,
-    state_rect: Rect,
-    state_v_scroll: usize,
-    state_h_scroll: usize,
+    summary_lines: Vec<String>,
+    summary_max_line_width: usize,
+    summary_rect: Rect,
+    summary_v_scroll: usize,
+    summary_h_scroll: usize,
+
+    trades_lines: Vec<String>,
+    trades_max_line_width: usize,
+    trades_rect: Rect,
+    trades_v_scroll: usize,
+    trades_h_scroll: usize,
 }
 
 pub struct LiveTuiView {
@@ -52,7 +59,7 @@ impl LiveTuiView {
             max_tui_log_len,
             state: Mutex::new(LiveTuiViewState {
                 log_file,
-                active_pane: ActivePane::StatePane,
+                active_pane: ActivePane::LogPane,
 
                 log_entries: Vec::new(),
                 log_max_line_width: 0,
@@ -60,11 +67,17 @@ impl LiveTuiView {
                 log_v_scroll: 0,
                 log_h_scroll: 0,
 
-                state_lines: vec!["Initializing...".to_string()],
-                state_max_line_width: 0,
-                state_rect: Rect::default(),
-                state_v_scroll: 0,
-                state_h_scroll: 0,
+                summary_lines: vec!["Initializing...".to_string()],
+                summary_max_line_width: 0,
+                summary_rect: Rect::default(),
+                summary_v_scroll: 0,
+                summary_h_scroll: 0,
+
+                trades_lines: vec!["Initializing...".to_string()],
+                trades_max_line_width: 0,
+                trades_rect: Rect::default(),
+                trades_v_scroll: 0,
+                trades_h_scroll: 0,
             }),
         })
     }
@@ -80,37 +93,56 @@ impl LiveTuiView {
         entries_len.saturating_sub(visible_height)
     }
 
-    // pub fn update_live_state(&self, state: String) {
-    //     let mut state_guard = self
-    //         .state
-    //         .lock()
-    //         .expect("`LiveTuiContent` mutex can't be poisoned");
+    pub fn update_summary(&self, state: String) {
+        let mut state_guard = self.get_state();
 
-    //     let mut new_lines = Vec::new();
+        let mut new_lines: Vec<String> = state.lines().map(|line| line.to_string()).collect();
+        new_lines.push("".to_string()); // Add empty line
 
-    //     // Split the state into lines for display
-    //     for line in state.lines() {
-    //         state_guard.state_max_line_width = state_guard.state_max_line_width.max(line.len());
-    //         new_lines.push(line.to_string());
-    //     }
+        state_guard.summary_max_line_width = new_lines.iter().map(|line| line.len()).max().unwrap();
 
-    //     new_lines.push("".to_string());
+        // Only reset scroll if the content structure has significantly changed
+        // or if current scroll position would be out of bounds
+        if new_lines.len() != state_guard.summary_lines.len() {
+            // If new content is shorter, adjust scroll to stay within bounds
+            if state_guard.summary_v_scroll >= new_lines.len() && new_lines.len() > 0 {
+                state_guard.summary_v_scroll = new_lines.len().saturating_sub(1);
+            }
+        }
 
-    //     if new_lines.len() != state_guard.state_lines.len() {
-    //         if state_guard.state_v_scroll >= new_lines.len() && new_lines.len() > 0 {
-    //             state_guard.state_v_scroll = new_lines.len().saturating_sub(1);
-    //         }
-    //     }
+        state_guard.summary_lines = new_lines;
+    }
 
-    //     state_guard.state_lines = new_lines;
-    // }
+    pub fn update_trades(&self, trades_table: String) {
+        let mut state_guard = self.get_state();
+
+        let mut new_lines: Vec<String> =
+            trades_table.lines().map(|line| line.to_string()).collect();
+        new_lines.push("".to_string()); // Add empty line
+
+        state_guard.trades_max_line_width = new_lines.iter().map(|line| line.len()).max().unwrap();
+
+        // Only reset scroll if the content structure has significantly changed
+        // or if current scroll position would be out of bounds
+        if new_lines.len() != state_guard.trades_lines.len() {
+            // If new content is shorter, adjust scroll to stay within bounds
+            if state_guard.trades_v_scroll >= new_lines.len() && new_lines.len() > 0 {
+                state_guard.trades_v_scroll = new_lines.len().saturating_sub(1);
+            }
+        }
+
+        state_guard.trades_lines = new_lines;
+    }
 
     pub fn scroll_up(&self) {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
-                state_guard.state_v_scroll = state_guard.state_v_scroll.saturating_sub(1)
+            ActivePane::TradesPane => {
+                state_guard.trades_v_scroll = state_guard.trades_v_scroll.saturating_sub(1)
+            }
+            ActivePane::SummaryPane => {
+                state_guard.summary_v_scroll = state_guard.summary_v_scroll.saturating_sub(1)
             }
             ActivePane::LogPane => {
                 state_guard.log_v_scroll = state_guard.log_v_scroll.saturating_sub(1)
@@ -122,11 +154,20 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
+            ActivePane::TradesPane => {
                 let max =
-                    Self::max_scroll_down(&state_guard.state_rect, state_guard.state_lines.len());
-                if state_guard.state_v_scroll < max {
-                    state_guard.state_v_scroll += 1;
+                    Self::max_scroll_down(&state_guard.trades_rect, state_guard.trades_lines.len());
+                if state_guard.trades_v_scroll < max {
+                    state_guard.trades_v_scroll += 1;
+                }
+            }
+            ActivePane::SummaryPane => {
+                let max = Self::max_scroll_down(
+                    &state_guard.summary_rect,
+                    state_guard.summary_lines.len(),
+                );
+                if state_guard.summary_v_scroll < max {
+                    state_guard.summary_v_scroll += 1;
                 }
             }
             ActivePane::LogPane => {
@@ -143,8 +184,11 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
-                state_guard.state_h_scroll = state_guard.state_h_scroll.saturating_sub(1);
+            ActivePane::TradesPane => {
+                state_guard.trades_h_scroll = state_guard.trades_h_scroll.saturating_sub(1);
+            }
+            ActivePane::SummaryPane => {
+                state_guard.summary_h_scroll = state_guard.summary_h_scroll.saturating_sub(1);
             }
             ActivePane::LogPane => {
                 state_guard.log_h_scroll = state_guard.log_h_scroll.saturating_sub(1);
@@ -161,13 +205,22 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
+            ActivePane::TradesPane => {
                 let max = Self::max_scroll_right(
-                    &state_guard.state_rect,
-                    state_guard.state_max_line_width,
+                    &state_guard.trades_rect,
+                    state_guard.trades_max_line_width,
                 );
-                if state_guard.state_h_scroll < max {
-                    state_guard.state_h_scroll += 1;
+                if state_guard.trades_h_scroll < max {
+                    state_guard.trades_h_scroll += 1;
+                }
+            }
+            ActivePane::SummaryPane => {
+                let max = Self::max_scroll_right(
+                    &state_guard.summary_rect,
+                    state_guard.summary_max_line_width,
+                );
+                if state_guard.summary_h_scroll < max {
+                    state_guard.summary_h_scroll += 1;
                 }
             }
             ActivePane::LogPane => {
@@ -184,9 +237,13 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
-                state_guard.state_v_scroll = 0;
-                state_guard.state_h_scroll = 0;
+            ActivePane::TradesPane => {
+                state_guard.trades_v_scroll = 0;
+                state_guard.trades_h_scroll = 0;
+            }
+            ActivePane::SummaryPane => {
+                state_guard.summary_v_scroll = 0;
+                state_guard.summary_h_scroll = 0;
             }
             ActivePane::LogPane => {
                 state_guard.log_v_scroll = 0;
@@ -199,10 +256,17 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         match state_guard.active_pane {
-            ActivePane::StatePane => {
-                state_guard.state_v_scroll =
-                    Self::max_scroll_down(&state_guard.state_rect, state_guard.state_lines.len());
-                state_guard.state_h_scroll = 0;
+            ActivePane::TradesPane => {
+                state_guard.trades_v_scroll =
+                    Self::max_scroll_down(&state_guard.trades_rect, state_guard.trades_lines.len());
+                state_guard.trades_h_scroll = 0;
+            }
+            ActivePane::SummaryPane => {
+                state_guard.summary_v_scroll = Self::max_scroll_down(
+                    &state_guard.summary_rect,
+                    state_guard.summary_lines.len(),
+                );
+                state_guard.summary_h_scroll = 0;
             }
             ActivePane::LogPane => {
                 state_guard.log_v_scroll =
@@ -216,8 +280,9 @@ impl LiveTuiView {
         let mut state_guard = self.get_state();
 
         state_guard.active_pane = match state_guard.active_pane {
-            ActivePane::StatePane => ActivePane::LogPane,
-            ActivePane::LogPane => ActivePane::StatePane,
+            ActivePane::TradesPane => ActivePane::LogPane,
+            ActivePane::LogPane => ActivePane::SummaryPane,
+            ActivePane::SummaryPane => ActivePane::TradesPane,
         };
     }
 
@@ -266,23 +331,38 @@ impl LiveTuiView {
         };
 
         let main_chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints([Constraint::Length(65), Constraint::Min(0)])
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
             .split(main_area);
+
+        let bottom_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Length(52), Constraint::Min(0)].as_ref())
+            .split(main_chunks[1]);
 
         let mut state_guard = self.get_state();
 
-        state_guard.state_rect = main_chunks[0];
-        state_guard.log_rect = main_chunks[1];
+        state_guard.trades_rect = main_chunks[0];
+        state_guard.summary_rect = bottom_chunks[0];
+        state_guard.log_rect = bottom_chunks[1];
 
         let state_list = Self::get_list(
-            "Live State",
-            &state_guard.state_lines,
-            state_guard.state_v_scroll,
-            state_guard.state_h_scroll,
-            state_guard.active_pane == ActivePane::StatePane,
+            "Trades",
+            &state_guard.trades_lines,
+            state_guard.trades_v_scroll,
+            state_guard.trades_h_scroll,
+            state_guard.active_pane == ActivePane::TradesPane,
         );
-        f.render_widget(state_list, state_guard.state_rect);
+        f.render_widget(state_list, state_guard.trades_rect);
+
+        let state_list = Self::get_list(
+            "Trades Summary",
+            &state_guard.summary_lines,
+            state_guard.summary_v_scroll,
+            state_guard.summary_h_scroll,
+            state_guard.active_pane == ActivePane::SummaryPane,
+        );
+        f.render_widget(state_list, state_guard.summary_rect);
 
         let log_list = Self::get_list(
             "Log",
