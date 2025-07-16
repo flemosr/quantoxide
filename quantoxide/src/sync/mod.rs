ee0651cf7d47a5e7e171c8712e98227c95f38338
Rafael Lemos <contact@flemosr.com>
2025-07-16 12:13:52 -0300

    quantoxide: adjust `sync` exports

--- quantoxide/src/sync/mod.rs
+++ quantoxide/src/sync/mod.rs
@@ -3,7 +3,7 @@ mod error;
 mod process;
 mod state;
 
-pub use engine::{SyncController, SyncEngine, SyncMode};
+pub use engine::{SyncConfig, SyncController, SyncEngine, SyncMode};
 pub use error::SyncError;
 pub use process::{PriceHistoryState, RealTimeCollectionError, SyncPriceHistoryError};
 pub use state::{SyncReader, SyncReceiver, SyncState, SyncStateNotSynced, SyncUpdate};
