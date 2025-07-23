951777f8192048be9e48e81e1cd72d8eaa506523
Rafael Lemos <contact@flemosr.com>
2025-07-23 20:54:30 -0300

    quantoxide: add `SimulatedTradeRunning::with_cash_in`

--- quantoxide/src/trade/backtest/executor/models/mod.rs
+++ quantoxide/src/trade/backtest/executor/models/mod.rs
@@ -192,6 +192,79 @@ impl SimulatedTradeRunning {
         }))
     }
 
+    pub fn with_cash_in(&self, market_price: Price, amount: NonZeroU64) -> Result<Arc<Self>> {
+        let amount = amount.get() as u64;
+        let current_pl = self.pl_estimate(market_price);
+
+        let (new_price, remaining_amount) = if current_pl > 0 {
+            if amount < current_pl as u64 {
+                // PL should be partially cashed-in. Calculate price that would
+                // correspond to the PL that will be extracted.
+                let new_price = trade_util::price_from_pl(
+                    self.side(),
+                    self.quantity(),
+                    self.entry_price().expect("must have `entry_price`"),
+                    amount as i64,
+                );
+                (new_price, 0)
+            } else {
+                // Whole PL should be cashed-in. Adjust trade price to market price
+                (market_price, amount - current_pl as u64)
+            }
+        } else {
+            // No PL to be cashed-in. Trade price shouldn't change
+            (self.price, amount)
+        };
+
+        let new_margin = if remaining_amount == 0 {
+            // Only PL will be cashed-in. Margin shouldn't change
+            self.margin()
+        } else {
+            Margin::try_from(self.margin().into_u64().saturating_sub(remaining_amount)).map_err(
+                |e| {
+                    SimulatedTradeExecutorError::Generic(format!(
+                        "cash-in would result in invalid margin: {e}"
+                    ))
+                },
+            )?
+        };
+
+        let new_leverage = Leverage::try_calculate(self.quantity(), new_margin, new_price)
+            .map_err(|e| {
+                SimulatedTradeExecutorError::Generic(format!(
+                    "cash-in would result in invalid leverage: {e}"
+                ))
+            })?;
+        let new_liquidation = trade_util::estimate_liquidation_price(
+            self.side,
+            self.quantity,
+            new_price,
+            new_leverage,
+        );
+
+        // TODO: Make stoploss optional with this case in mind
+        let new_stoploss = match self.side {
+            TradeSide::Buy => new_liquidation.max(self.stoploss),
+            TradeSide::Sell => new_liquidation.min(self.stoploss),
+        };
+
+        Ok(Arc::new(Self {
+            id: self.id,
+            side: self.side,
+            entry_time: self.entry_time,
+            entry_price: self.entry_price,
+            price: new_price,
+            stoploss: new_stoploss,
+            takeprofit: self.takeprofit,
+            margin: new_margin,
+            quantity: self.quantity,
+            leverage: new_leverage,
+            liquidation: new_liquidation,
+            opening_fee: self.opening_fee,
+            closing_fee_reserved: self.closing_fee_reserved,
+        }))
+    }
+
     #[cfg(test)]
     fn closing_fee_est(&self, fee_perc: BoundedPercentage, close_price: Price) -> u64 {
         let fee_calc = SATS_PER_BTC * fee_perc.into_f64() / 100.;
