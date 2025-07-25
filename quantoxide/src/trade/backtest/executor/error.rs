7e281044c7b692c0bff8a7248352bbdd4fa8f6ea
Rafael Lemos <contact@flemosr.com>
2025-07-24 22:06:43 -0300

    quantoxide: adjust `SimulatedTradeExecutorError`

--- quantoxide/src/trade/backtest/executor/error.rs
+++ quantoxide/src/trade/backtest/executor/error.rs
@@ -4,8 +4,8 @@ use chrono::{DateTime, Utc};
 use thiserror::Error;
 
 use lnm_sdk::api::rest::models::{
-    Price, TradeSide,
-    error::{PriceValidationError, ValidationError},
+    TradeSide,
+    error::{PriceValidationError, TradeValidationError, ValidationError},
 };
 
 use crate::db::{error::DbError, models::PriceHistoryEntry};
@@ -15,40 +15,11 @@ pub enum SimulatedTradeExecutorError {
     #[error("[PriceValidation] {0}")]
     PriceValidation(#[from] PriceValidationError),
 
+    #[error("TradeValidation error {0}")]
+    TradeValidation(#[from] TradeValidationError),
+
     #[error("[Validation] {0}")]
     ValidationError(#[from] ValidationError),
-
-    #[error(
-        "Stoploss ({stoploss}) can't be below liquidation price ({liquidation}) for long positions"
-    )]
-    StoplossBelowLiquidationLong { stoploss: Price, liquidation: Price },
-
-    #[error("Stoploss ({stoploss}) can't be above entry price ({entry_price}) for long positions")]
-    StoplossAboveEntryForLong { stoploss: Price, entry_price: Price },
-
-    #[error(
-        "Takeprofit ({takeprofit}) can't be below entry price ({entry_price}) for long positions"
-    )]
-    TakeprofitBelowEntryForLong {
-        takeprofit: Price,
-        entry_price: Price,
-    },
-
-    #[error(
-        "Stoploss ({stoploss}) can't be above liquidation price ({liquidation}) for short positions"
-    )]
-    StoplossAboveLiquidationShort { stoploss: Price, liquidation: Price },
-
-    #[error("Stoploss ({stoploss}) can't be below entry price ({entry_price}) for short positions")]
-    StoplossBelowEntryForShort { stoploss: Price, entry_price: Price },
-
-    #[error(
-        "Takeprofit ({takeprofit}) can't be above entry price ({entry_price}) for short positions"
-    )]
-    TakeprofitAboveEntryForShort {
-        takeprofit: Price,
-        entry_price: Price,
-    },
 
     #[error("Invalid time sequence: new time {new_time} is not after current time {current_time}")]
     TimeSequenceViolation {
