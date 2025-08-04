c017e18eb99f3d00f58a85d21a4b6eb29bac9436
Rafael Lemos <contact@flemosr.com>
2025-08-03 22:42:52 -0300

    quantoxide: additional `SimulatedTradeExecutor` tests

--- quantoxide/src/trade/backtest/executor/tests.rs
+++ quantoxide/src/trade/backtest/executor/tests.rs
@@ -671,3 +671,398 @@ async fn test_simulated_trade_executor_trailing_stoploss_short() {
     assert_eq!(state.running_short_len(), 0); // Position closed
     assert_eq!(state.closed_len(), 1);
 }
+
+#[tokio::test]
+async fn test_simulated_trade_executor_partial_cash_in_short_profit() -> Result<()> {
+    // Step 1: Create a new executor with market price as 100_000, balance of 1_000_000
+
+    let start_time = Utc::now();
+    let market_price = 100_000.0;
+    let start_balance = 1_000_000;
+    let fee_perc = BoundedPercentage::try_from(0.1).unwrap(); // 0.1% fee
+    let tsl_step_size = BoundedPercentage::try_from(0.1).unwrap(); // 0.1% trailing stop loss step size
+    let max_running_qtd = 10;
+
+    let executor = SimulatedTradeExecutor::new(
+        max_running_qtd,
+        fee_perc,
+        tsl_step_size,
+        start_time,
+        market_price,
+        start_balance,
+    );
+
+    let state = executor.trading_state().await?;
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), start_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), None);
+    assert_eq!(state.running_long_len(), 0);
+    assert_eq!(state.running_long_margin(), 0);
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 2: Open a short trade
+
+    let size = Quantity::try_from(500).unwrap().into();
+    let leverage = Leverage::try_from(1).unwrap();
+    let stoploss = None;
+    let takeprofit = None;
+
+    executor
+        .open_short(size, leverage, stoploss, takeprofit)
+        .await?;
+
+    let state = executor.trading_state().await?;
+    let expected_balance = start_balance - state.running_short_margin() - state.running_fees();
+
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_long_len(), 0);
+    assert_eq!(state.running_long_margin(), 0);
+    assert_eq!(state.running_short_len(), 1);
+    assert!(
+        state.running_short_margin() > 0,
+        "Short margin should be positive"
+    );
+    assert_eq!(state.running_pl(), 0); // No PL yet since price hasn't changed
+    assert!(state.running_fees() > 0, "Trading fees should be estimated");
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 3: Update price to 98_000 (2% drop)
+
+    let time = start_time + Duration::seconds(1);
+    let market_price = 98_000.0;
+    executor.tick_update(time, market_price).await?;
+
+    let state = executor.trading_state().await?;
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 500_500);
+    assert_eq!(state.running_pl(), 10_204);
+    assert_eq!(state.running_fees(), 500);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 4: Cash_in
+
+    let state = executor.trading_state().await?;
+    let (trade, _) = state.running().values().next().expect("has running trade");
+
+    let cash_in = 5000;
+
+    executor
+        .cash_in(trade.id(), cash_in.try_into().unwrap())
+        .await?;
+
+    let state = executor.trading_state().await?;
+
+    let expected_balance =
+        start_balance - state.running_short_margin() - state.running_fees() + cash_in;
+
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 500_500);
+    assert_eq!(state.running_pl(), 10_204 - cash_in as i64);
+    assert_eq!(state.running_fees(), 500);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 4_999);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 5: Close trade
+
+    executor.close_trade(trade.id()).await?;
+
+    let state = executor.trading_state().await?;
+
+    let expected_balance =
+        (start_balance as i64 + state.closed_pl() - state.closed_fees() as i64) as u64;
+
+    assert_eq!(state.last_tick_time(), time);
+    assert!((state.balance() as i64 - expected_balance as i64).abs() < 2);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(time));
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 1);
+    assert_eq!(state.closed_pl(), 10_203);
+    assert_eq!(state.closed_fees(), 1_010);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_simulated_trade_executor_full_cash_in_short_profit() -> Result<()> {
+    // Step 1: Create a new executor with market price as 100_000, balance of 1_000_000
+
+    let start_time = Utc::now();
+    let market_price = 100_000.0;
+    let start_balance = 1_000_000;
+    let fee_perc = BoundedPercentage::try_from(0.1).unwrap(); // 0.1% fee
+    let tsl_step_size = BoundedPercentage::try_from(0.1).unwrap(); // 0.1% trailing stop loss step size
+    let max_running_qtd = 10;
+
+    let executor = SimulatedTradeExecutor::new(
+        max_running_qtd,
+        fee_perc,
+        tsl_step_size,
+        start_time,
+        market_price,
+        start_balance,
+    );
+
+    let state = executor.trading_state().await?;
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), start_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), None);
+    assert_eq!(state.running_long_len(), 0);
+    assert_eq!(state.running_long_margin(), 0);
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 2: Open a short trade
+
+    let size = Quantity::try_from(500).unwrap().into();
+    let leverage = Leverage::try_from(10).unwrap();
+    let stoploss = None;
+    let takeprofit = None;
+
+    executor
+        .open_short(size, leverage, stoploss, takeprofit)
+        .await?;
+
+    let state = executor.trading_state().await?;
+    let expected_balance = start_balance - state.running_short_margin() - state.running_fees();
+
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_long_len(), 0);
+    assert_eq!(state.running_long_margin(), 0);
+    assert_eq!(state.running_short_len(), 1);
+    assert!(
+        state.running_short_margin() > 0,
+        "Short margin should be positive"
+    );
+    assert_eq!(state.running_pl(), 0); // No PL yet since price hasn't changed
+    assert!(state.running_fees() > 0, "Trading fees should be estimated");
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 3: Update price to 98_000 (2% drop)
+
+    let time = start_time + Duration::seconds(1);
+    let market_price = 98_000.0;
+    executor.tick_update(time, market_price).await?;
+
+    let state = executor.trading_state().await?;
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 50_950);
+    assert_eq!(state.running_pl(), 10_204);
+    assert_eq!(state.running_fees(), 500);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 4: Cash_in
+
+    let state = executor.trading_state().await?;
+    let (trade, _) = state.running().values().next().expect("has running trade");
+
+    let cash_in = 15_000;
+
+    executor
+        .cash_in(trade.id(), cash_in.try_into().unwrap())
+        .await?;
+
+    let state = executor.trading_state().await?;
+
+    let expected_balance =
+        start_balance - state.running_short_margin() - state.running_fees() + 10_204;
+
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 50_950 - cash_in + 10_204);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 500);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 10_204);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 5: Close trade
+
+    executor.close_trade(trade.id()).await?;
+
+    let state = executor.trading_state().await?;
+
+    let expected_balance = start_balance as i64 + state.closed_pl() - state.closed_fees() as i64;
+
+    assert_eq!(state.last_tick_time(), time);
+    assert!((state.balance() as i64 - expected_balance).abs() < 2);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(time));
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 1);
+    assert_eq!(state.closed_pl(), 10_204);
+    assert_eq!(state.closed_fees(), 1_010);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_simulated_trade_executor_add_margin_short_loss() -> Result<()> {
+    // Step 1: Create a new executor with market price as 100_000, balance of 1_000_000
+
+    let start_time = Utc::now();
+    let market_price = 100_000.0;
+    let start_balance = 1_000_000;
+    let fee_perc = BoundedPercentage::try_from(0.1).unwrap(); // 0.1% fee
+    let tsl_step_size = BoundedPercentage::try_from(0.1).unwrap();
+    let max_running_qtd = 10;
+
+    let executor = SimulatedTradeExecutor::new(
+        max_running_qtd,
+        fee_perc,
+        tsl_step_size,
+        start_time,
+        market_price,
+        start_balance,
+    );
+
+    let state = executor.trading_state().await?;
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), start_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), None);
+    assert_eq!(state.running_long_len(), 0);
+    assert_eq!(state.running_long_margin(), 0);
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 0);
+    assert_eq!(state.closed_pl(), 0);
+    assert_eq!(state.closed_fees(), 0);
+
+    // Step 2: Open a short trade
+
+    let size = Quantity::try_from(500).unwrap().into();
+    let leverage = Leverage::try_from(10).unwrap();
+    let stoploss_price = Price::clamp_from(102_000.);
+    let stoploss = Some(Stoploss::fixed(stoploss_price));
+    let takeprofit = Some(Price::clamp_from(98_000.));
+
+    executor
+        .open_short(size, leverage, stoploss, takeprofit)
+        .await?;
+
+    let state = executor.trading_state().await?;
+    let expected_balance = start_balance - state.running_short_margin() - state.running_fees();
+
+    assert_eq!(state.last_tick_time(), start_time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 50_950);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 500);
+    assert_eq!(state.closed_len(), 0);
+
+    // Step 3: Update price to 101_000 (1% increase)
+
+    let time = start_time + Duration::seconds(1);
+    let market_price = 101_000.0;
+    executor.tick_update(time, market_price).await?;
+
+    let state = executor.trading_state().await?;
+    let (trade, _) = state.running().values().next().expect("has running trade");
+
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 50_950);
+    assert_eq!(state.running_pl(), -4_951);
+    assert_eq!(state.closed_len(), 0);
+
+    // Step 4: Add margin
+
+    let add_margin = 5_000;
+    executor
+        .add_margin(trade.id(), add_margin.try_into().unwrap())
+        .await?;
+    let state = executor.trading_state().await?;
+
+    let expected_balance = start_balance - state.running_short_margin() - state.running_fees();
+
+    assert_eq!(state.last_tick_time(), time);
+    assert_eq!(state.balance(), expected_balance);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(start_time));
+    assert_eq!(state.running_short_len(), 1);
+    assert_eq!(state.running_short_margin(), 50_950 + add_margin);
+    assert_eq!(state.running_pl(), -4_951);
+    assert_eq!(state.closed_len(), 0);
+
+    // Step 5: Close trade
+
+    executor.close_trade(trade.id()).await?;
+
+    let state = executor.trading_state().await?;
+
+    let expected_balance =
+        (start_balance as i64 + state.closed_pl() - state.closed_fees() as i64) as u64;
+
+    assert_eq!(state.last_tick_time(), time);
+    assert!((state.balance() as i64 - expected_balance as i64).abs() < 2);
+    assert_eq!(state.market_price().into_f64(), market_price);
+    assert_eq!(state.last_trade_time(), Some(time));
+    assert_eq!(state.running_short_len(), 0);
+    assert_eq!(state.running_short_margin(), 0);
+    assert_eq!(state.running_pl(), 0);
+    assert_eq!(state.running_fees(), 0);
+    assert_eq!(state.closed_len(), 1);
+    assert_eq!(state.closed_pl(), -4_951);
+    assert_eq!(state.closed_fees(), 995);
+
+    Ok(())
+}
