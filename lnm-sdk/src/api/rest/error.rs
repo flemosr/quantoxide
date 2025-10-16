f69a918ee2ec7bbc68ec53d111c5c490992e30a3
Rafael Lemos <contact@flemosr.com>
2025-10-15 21:35:38 -0300

    lnm-sdk: adjust `RestApiError` enum
    Remove `Generic`, adjust previous and add new variants.

--- lnm-sdk/src/api/rest/error.rs
+++ lnm-sdk/src/api/rest/error.rs
@@ -1,16 +1,51 @@
 use std::result;
+
+use hmac::digest::InvalidLength;
+use hyper::{Method, StatusCode, header::InvalidHeaderValue};
 use thiserror::Error;
+
+use super::models::error::FuturesTradeRequestValidationError;
 
 #[derive(Error, Debug)]
 pub enum RestApiError {
     #[error("Url parse error: {0}")]
     UrlParse(String),
-    #[error("Response error: {0}")]
-    Response(reqwest::Error),
+
     #[error("Unexpected schema error: {0}")]
     UnexpectedSchema(reqwest::Error),
-    #[error("RestApi generic error: {0}")]
-    Generic(String),
+
+    #[error("Invalid futures trade request error: {0}")]
+    FuturesTradeRequestValidation(FuturesTradeRequestValidationError),
+
+    #[error("Invalid header value error: {0}")]
+    InvalidHeaderValue(#[from] InvalidHeaderValue),
+
+    #[error("Invalid secret HMAC error: {0}")]
+    InvalidSecretHmac(InvalidLength),
+
+    #[error("Generic `reqwest` error: {0}")]
+    GenericReqwest(reqwest::Error),
+
+    #[error("Authentication required for request but no credentials provided")]
+    MissingRequestCredentials,
+
+    #[error("Tried to make a request with unsupported method: {0}")]
+    UnsupportedMethod(Method),
+
+    #[error("Failed to send request error: {0}")]
+    SendFailed(reqwest::Error),
+
+    #[error("Received error response. Status: {status}, text: {text}")]
+    ErrorResponse { status: StatusCode, text: String },
+
+    #[error("Response JSON deserialization failed. Raw response: '{raw_response}', error: {e}")]
+    ResponseJsonDeserializeFailed {
+        raw_response: String,
+        e: serde_json::Error,
+    },
+
+    #[error("Request JSON serialization failed. Error: {0}")]
+    RequestJsonSerializeFailed(serde_json::Error),
 }
 
 pub type Result<T> = result::Result<T, RestApiError>;
