bbf6264d9aaa08601e9f394838ed2f24b7acd04d
Rafael Lemos <contact@flemosr.com>
2025-10-19 20:58:52 -0300

    quantoxide: adjust `SignalError` enum
    Remove `Generic`, add new variants.

--- quantoxide/src/signal/error.rs
+++ quantoxide/src/signal/error.rs
@@ -1,20 +1,58 @@
 use std::{result, sync::Arc};
 
 use thiserror::Error;
-use tokio::{sync::broadcast::error::SendError, task::JoinError};
+use tokio::{
+    sync::broadcast::error::{RecvError, SendError},
+    task::JoinError,
+};
+
+use crate::{db::error::DbError, util::PanicPayload};
 
 use super::live::LiveSignalStatus;
 
 #[derive(Error, Debug)]
 pub enum SignalError {
-    #[error("Generic error, {0}")]
-    Generic(String),
-
     #[error("SignalTransmiterFailed failed error {0}")]
     SignalTransmiterFailed(SendError<Arc<LiveSignalStatus>>),
 
     #[error("TaskJoin error {0}")]
     TaskJoin(JoinError),
+
+    #[error("It was not possible to convert `evaluation_interval_secs` to `NonZeroU64`")]
+    InvalidEvaluationInterval,
+
+    #[error("`SignalEvaluator::evaluate` panicked: {0}")]
+    EvaluatePanicked(PanicPayload),
+
+    #[error("`SignalEvaluator::evaluate` error: {0}")]
+    EvaluateError(String),
+
+    #[error("`SignalName` cannot be an empty `String`")]
+    InvalidSignalNameEmptyString,
+
+    #[error("[Db] {0}")]
+    Db(#[from] DbError),
+
+    #[error("`Sync` process (dependency) was shutdown")]
+    SyncProcessShutdown,
+
+    #[error("`Sync` `RecvError` error: {0}")]
+    SyncRecv(RecvError),
+
+    #[error("Shutdown `RecvError` error: {0}")]
+    ShutdownRecv(RecvError),
+
+    #[error("Live Signal already shutdown error")]
+    LiveSignalAlreadyShutdown,
+
+    #[error("Failed to send live signal process shutdown request error: {0}")]
+    SendShutdownFailed(SendError<()>),
+
+    #[error("Live Signal shutdown timeout error")]
+    ShutdownTimeout,
+
+    #[error("At least one signal evaluator must be provided")]
+    EmptyEvaluatorsVec,
 }
 
 impl PartialEq for SignalError {
