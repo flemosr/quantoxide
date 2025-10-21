9be14d20357b9789e935629dc4fa8c6f73eb8982
Rafael Lemos <contact@flemosr.com>
2025-10-20 21:54:24 -0300

    quantoxide: adjust `LiveError` enum
    Remove `Generic`, add new variants.

--- quantoxide/src/trade/live/error.rs
+++ quantoxide/src/trade/live/error.rs
@@ -1,28 +1,73 @@
 use std::result;
 
 use thiserror::Error;
-use tokio::task::JoinError;
+use tokio::{
+    sync::broadcast::error::{RecvError, SendError},
+    task::JoinError,
+};
 
-use super::executor::error::LiveTradeExecutorError;
+use crate::{db::error::DbError, signal::error::SignalError, sync::SyncError};
+
+use super::{super::error::TradeCoreError, executor::error::LiveTradeExecutorError};
 
 #[derive(Error, Debug)]
 pub enum LiveError {
+    #[error("[Db] {0}")]
+    Db(#[from] DbError),
+
     #[error("[TaskJoin] {0}")]
     TaskJoin(JoinError),
 
     #[error("Launch executor error {0}")]
     LauchExecutor(LiveTradeExecutorError),
 
-    #[error("Generic error, {0}")]
-    Generic(String),
-}
+    #[error("Operator error: {0}")]
+    OperatorError(TradeCoreError),
 
-impl PartialEq for LiveError {
-    fn eq(&self, other: &Self) -> bool {
-        self.to_string() == other.to_string()
-    }
-}
+    #[error("`Sync` process (dependency) was shutdown")]
+    SyncProcessShutdown,
 
-impl Eq for LiveError {}
+    #[error("`Sync` `RecvError` error: {0}")]
+    SyncRecv(RecvError),
+
+    #[error("`LiveSignal` process (dependency) was shutdown")]
+    SignalProcessShutdown,
+
+    #[error("`LiveSignal` `RecvError` error: {0}")]
+    SignalRecv(RecvError),
+
+    #[error("`LiveTradeExecutor` `RecvError` error: {0}")]
+    ExecutorRecv(RecvError),
+
+    #[error("[ShutdownRecv] {0}")]
+    ShutdownRecv(RecvError),
+
+    #[error("Live trade process already shutdown error")]
+    LiveAlreadyShutdown,
+
+    #[error("Failed to send live trade process shutdown request error: {0}")]
+    SendShutdownFailed(SendError<()>),
+
+    #[error("Live shutdown timeout error")]
+    ShutdownTimeout,
+
+    #[error("`LiveTradeExecutor` shutdown error: {0}")]
+    ExecutorShutdownError(LiveTradeExecutorError),
+
+    #[error("`LiveSignal` shutdown error: {0}")]
+    SignalShutdown(SignalError),
+
+    #[error("`Sync` shutdown error: {0}")]
+    SyncShutdown(SyncError),
+
+    #[error("Lauch `LiveSignal` error: {0}")]
+    LaunchLiveSignalEngine(SignalError),
+
+    #[error("Operator iteration time too long for iteration interval")]
+    OperatorIterationTimeTooLong,
+
+    #[error("At least one signal evaluator must be provided")]
+    EmptyEvaluatorsVec,
+}
 
 pub type Result<T> = result::Result<T, LiveError>;
