2e03e62747f486d486ca677a06e87751fe16962a
Rafael Lemos <contact@flemosr.com>
2025-10-21 12:15:56 -0300

    quantoxide: adjust `TuiError` enum
    Remove `Generic`, add new variants.

--- quantoxide/src/tui/error.rs
+++ quantoxide/src/tui/error.rs
@@ -1,11 +1,103 @@
-use std::result;
+use std::{io, result};
 
 use thiserror::Error;
+use tokio::{
+    sync::{broadcast::error::RecvError, mpsc::error::SendError},
+    task::JoinError,
+};
 
-#[derive(Error, Debug, PartialEq)]
+use crate::{
+    sync::SyncError,
+    trade::{error::TradeCoreError, live::error::LiveError},
+};
+
+use super::{TuiStatus, backtest::BacktestUiMessage, live::LiveUiMessage, sync::SyncUiMessage};
+
+#[derive(Error, Debug)]
 pub enum TuiError {
-    #[error("Generic error, {0}")]
-    Generic(String),
+    #[error("TUI not running error: {0}")]
+    TuiNotRunning(TuiStatus),
+
+    #[error("Terminal setup error: {0}")]
+    TerminalSetup(io::Error),
+
+    #[error("Terminal restore error: {0}")]
+    TerminalRestore(io::Error),
+
+    #[error("Terminal event read error, {0}")]
+    TerminalEventRead(io::Error),
+
+    #[error("Draw failed, terminal already restored")]
+    DrawTerminalAlreadyRestored,
+
+    #[error("Draw failed error: {0}")]
+    DrawFailed(io::Error),
+
+    #[error("Open log file error: {0}")]
+    LogFileOpen(io::Error),
+
+    #[error("Write to log file error: {0}")]
+    LogFileWrite(io::Error),
+
+    #[error("Failed to send TUI process shutdown request error: {0}")]
+    SendShutdownFailed(SendError<()>),
+
+    #[error("TUI already shutdown error")]
+    TuiAlreadyShutdown,
+
+    #[error("TUI crashed without status update error")]
+    TuiCrashedWithoutStatusUpdate,
+
+    #[error("Failed to send shutdown completed signal error: {0}")]
+    SendShutdownCompletedFailed(String),
+
+    #[error("TaskJoin error {0}")]
+    TaskJoin(JoinError),
+
+    #[error("TUI shutdown timeout error")]
+    ShutdownTimeout,
+
+    #[error("TUI shutdown failed: {0}")]
+    ShutdownFailed(String),
+
+    #[error("Sync TUI send failed: {0}")]
+    SyncTuiSendFailed(SendError<SyncUiMessage>),
+
+    #[error("Sync recv error: {0}")]
+    SyncRecv(RecvError),
+
+    #[error("Sync engine already coupled")]
+    SyncEngineAlreadyCoupled,
+
+    #[error("Sync shutdown failed: {0}")]
+    SyncShutdownFailed(SyncError),
+
+    #[error("Live TUI send failed: {0}")]
+    LiveTuiSendFailed(SendError<LiveUiMessage>),
+
+    #[error("Live handle closed trade failed: {0}")]
+    LiveHandleClosedTradeFailed(TradeCoreError),
+
+    #[error("Live recv error: {0}")]
+    LiveRecv(RecvError),
+
+    #[error("Live engine already coupled")]
+    LiveEngineAlreadyCoupled,
+
+    #[error("Live engine start failed: {0}")]
+    LiveEngineStartFailed(LiveError),
+
+    #[error("Live shutdown failed: {0}")]
+    LiveShutdownFailed(LiveError),
+
+    #[error("Backtest TUI send failed: {0}")]
+    BacktestTuiSendFailed(SendError<BacktestUiMessage>),
+
+    #[error("Backtest recv error: {0}")]
+    BacktestRecv(RecvError),
+
+    #[error("Backtest engine already coupled")]
+    BacktestEngineAlreadyCoupled,
 }
 
 pub type Result<T> = result::Result<T, TuiError>;
