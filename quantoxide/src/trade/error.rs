6ce66e54946fb1959ac4be5ac3a18612b798015f
Rafael Lemos <contact@flemosr.com>
2025-10-21 10:42:41 -0300

    quantoxide: adjust `TradeCoreError` enum
    Remove `Generic`, add new variants.

--- quantoxide/src/trade/error.rs
+++ quantoxide/src/trade/error.rs
@@ -1,6 +1,13 @@
 use std::result;
 
 use thiserror::Error;
+use uuid::Uuid;
+
+use lnm_sdk::api::rest::models::{
+    BoundedPercentage, LowerBoundedPercentage, Price, error::PriceValidationError,
+};
+
+use crate::util::PanicPayload;
 
 use super::{
     backtest::error::SimulatedTradeExecutorError, live::executor::error::LiveTradeExecutorError,
@@ -19,8 +26,64 @@ pub type TradeExecutorResult<T> = result::Result<T, TradeExecutorError>;
 
 #[derive(Error, Debug)]
 pub enum TradeCoreError {
-    #[error("Generic error, {0}")]
-    Generic(String),
+    #[error("Trade {trade_id} is not closed")]
+    TradeNotClosed { trade_id: Uuid },
+
+    #[error("Trailing stoploss {tsl} cannot be smaller than step size {tsl_step_size}")]
+    InvalidStoplossSmallerThanTrailingStepSize {
+        tsl: BoundedPercentage,
+        tsl_step_size: BoundedPercentage,
+    },
+
+    #[error("Invalid price resulting from applying discount {discount} to price {price}: {e}")]
+    InvalidPriceApplyDiscount {
+        price: Price,
+        discount: BoundedPercentage,
+        e: PriceValidationError,
+    },
+
+    #[error("Invalid price resulting from applying gain {gain} to price {price}: {e}")]
+    InvalidPriceApplyGain {
+        price: Price,
+        gain: LowerBoundedPercentage,
+        e: PriceValidationError,
+    },
+
+    #[error("Invalid price resulting from rounding {price}: {e}")]
+    InvalidPriceRounding { price: f64, e: PriceValidationError },
+
+    #[error("`SignalOperator::set_trade_executor` panicked: {0}")]
+    SignalOperatorSetTradeExecutorPanicked(PanicPayload),
+
+    #[error("`SignalOperator::set_trade_executor` error: {0}")]
+    SignalOperatorSetTradeExecutorError(String),
+
+    #[error("`SignalOperator::process_signal` panicked: {0}")]
+    SignalOperatorProcessSignalPanicked(PanicPayload),
+
+    #[error("`SignalOperator::process_signal` error: {0}")]
+    SignalOperatorProcessSignalError(String),
+
+    #[error("`RawOperator::set_trade_executor` panicked: {0}")]
+    RawOperatorSetTradeExecutorPanicked(PanicPayload),
+
+    #[error("`RawOperator::set_trade_executor` error: {0}")]
+    RawOperatorSetTradeExecutorError(String),
+
+    #[error("`RawOperator::iteration_interval_secs` panicked: {0}")]
+    RawOperatorIterationIntervalPanicked(PanicPayload),
+
+    #[error("`RawOperator::context_window_secs` panicked: {0}")]
+    RawOperatorContextWindowPanicked(PanicPayload),
+
+    #[error("`RawOperator::iterate` panicked: {0}")]
+    RawOperatorIteratePanicked(PanicPayload),
+
+    #[error("`RawOperator::iterate` error: {0}")]
+    RawOperatorIterateError(String),
+
+    #[error("Tried to evaluate next update trigger of trade {trade_id} without stoploss")]
+    NoNextTriggerTradeStoplossNotSet { trade_id: Uuid },
 }
 
 pub type TradeCoreResult<T> = result::Result<T, TradeCoreError>;
