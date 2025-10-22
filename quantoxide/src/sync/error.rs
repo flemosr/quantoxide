71255fb39cdd48ca537a6b84037e23bdddbfd1da
Rafael Lemos <contact@flemosr.com>
2025-10-21 22:05:08 -0300

    quantoxide: adjust `SyncError`
    Remove `SyncProcessError` variants.

--- quantoxide/src/sync/error.rs
+++ quantoxide/src/sync/error.rs
@@ -1,52 +1,21 @@
-use std::{result, sync::Arc};
+use std::result;
 
 use thiserror::Error;
-use tokio::{
-    sync::broadcast::error::{RecvError, SendError},
-    task::JoinError,
-};
-
-use super::{RealTimeCollectionError, SyncPriceHistoryError, SyncStatus};
+use tokio::{sync::broadcast::error::SendError, task::JoinError};
 
 #[derive(Error, Debug)]
 pub enum SyncError {
-    #[error("[SyncPriceHistory] {0}")]
-    SyncPriceHistory(#[from] SyncPriceHistoryError),
-
-    #[error("[RealTimeCollection] {0}")]
-    RealTimeCollection(#[from] RealTimeCollectionError),
-
-    #[error("[SyncTransmiterFailed] {0}")]
-    SyncTransmiterFailed(SendError<Arc<SyncStatus>>),
-
-    #[error("[TaskJoin] {0}")]
-    TaskJoin(JoinError),
-
-    #[error("[ShutdownRecv] {0}")]
-    ShutdownRecv(RecvError),
-
-    #[error("UnexpectedRealTimeCollectionShutdown error")]
-    UnexpectedRealTimeCollectionShutdown,
-
-    #[error("PriceTickRecv error: {0}")]
-    PriceTickRecv(RecvError),
+    #[error("[SyncProcessTaskJoin] {0}")]
+    SyncProcessTaskJoin(JoinError),
 
     #[error("Sync already shutdown error")]
     SyncAlreadyShutdown,
 
-    #[error("Failed to send sync process shutdown request error: {0}")]
-    SendShutdownFailed(SendError<()>),
+    #[error("Failed to send sync shutdown signal error: {0}")]
+    SendShutdownSignalFailed(SendError<()>),
 
     #[error("Sync shutdown timeout error")]
     ShutdownTimeout,
 }
-
-impl PartialEq for SyncError {
-    fn eq(&self, other: &Self) -> bool {
-        self.to_string() == other.to_string()
-    }
-}
-
-impl Eq for SyncError {}
 
 pub type Result<T> = result::Result<T, SyncError>;
