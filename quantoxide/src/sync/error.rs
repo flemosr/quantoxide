1a186b4bbd0301c584c0c06b10325806da987388
Rafael Lemos <contact@flemosr.com>
2025-10-28 09:21:50 -0300

    quantoxide: add `SyncError::SyncAlreadyTerminated` variant

--- quantoxide/src/sync/error.rs
+++ quantoxide/src/sync/error.rs
@@ -2,12 +2,17 @@ use std::{result, sync::Arc};
 
 use thiserror::Error;
 
+use crate::sync::SyncStatus;
+
 use super::process::SyncProcessFatalError;
 
 #[derive(Error, Debug)]
 pub enum SyncError {
     #[error("Sync already shutdown error")]
     SyncAlreadyShutdown,
+
+    #[error("Sync process already terminated error, status: {0}")]
+    SyncAlreadyTerminated(SyncStatus),
 
     #[error("Sync shutdown procedure failed: {0}")]
     SyncShutdownFailed(Arc<SyncProcessFatalError>),
