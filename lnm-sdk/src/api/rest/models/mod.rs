70b8fc841d25a1fc8020255cad9a1c3c0061d94c
Rafael Lemos <contact@flemosr.com>
2025-10-31 12:07:31 -0300

    lnm-sdk: adjust `api::rest::models` API, main mod

--- lnm-sdk/src/api/rest/models/mod.rs
+++ lnm-sdk/src/api/rest/models/mod.rs
@@ -1,25 +1,12 @@
 pub const SATS_PER_BTC: f64 = 100_000_000.;
 
-pub mod error;
-mod leverage;
-mod margin;
-mod price;
-mod price_history;
-mod quantity;
-mod serde_util;
-mod ticker;
-mod trade;
-mod user;
-
-pub use leverage::Leverage;
-pub use margin::Margin;
-pub use price::{BoundedPercentage, LowerBoundedPercentage, Price};
-pub use price_history::PriceEntryLNM;
-pub use quantity::Quantity;
-pub use ticker::Ticker;
-pub use trade::{
-    FuturesTradeRequestBody, FuturesUpdateTradeRequestBody, LnmTrade, NestedTradesResponse, Trade,
-    TradeClosed, TradeExecution, TradeExecutionType, TradeRunning, TradeSide, TradeSize,
-    TradeStatus, TradeUpdateType, Uuid, util as trade_util,
-};
-pub use user::{User, UserRole};
+pub(crate) mod error;
+pub(crate) mod leverage;
+pub(crate) mod margin;
+pub(crate) mod price;
+pub(crate) mod price_history;
+pub(crate) mod quantity;
+pub(crate) mod serde_util;
+pub(crate) mod ticker;
+pub(crate) mod trade;
+pub(crate) mod user;
