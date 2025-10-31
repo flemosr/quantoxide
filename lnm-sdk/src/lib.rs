90b064a193cdc74101990e6ea4d461e825d7bcce
Rafael Lemos <contact@flemosr.com>
2025-10-31 12:15:06 -0300

    lnm-sdk: adjust `lib.rs` API, WIP

--- lnm-sdk/src/lib.rs
+++ lnm-sdk/src/lib.rs
@@ -1,1 +1,27 @@
 pub mod api;
+
+pub mod error {
+    pub use super::api::rest::models::error::{
+        BoundedPercentageValidationError, FuturesTradeRequestValidationError,
+        LeverageValidationError, LowerBoundedPercentageValidationError, MarginValidationError,
+        PriceValidationError, QuantityValidationError, TradeValidationError, ValidationError,
+    };
+}
+
+pub mod models {
+    pub use uuid::Uuid;
+
+    pub use super::api::rest::models::{
+        leverage::Leverage,
+        margin::Margin,
+        price::{BoundedPercentage, LowerBoundedPercentage, Price},
+        price_history::PriceEntryLNM,
+        quantity::Quantity,
+        ticker::Ticker,
+        trade::{
+            LnmTrade, Trade, TradeClosed, TradeExecution, TradeExecutionType, TradeRunning,
+            TradeSide, TradeSize, TradeStatus, util as trade_util,
+        },
+        user::{User, UserRole},
+    };
+}
