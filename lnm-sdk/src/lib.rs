e4be22adad7270cfa61483c0474d39a9e736e929
Rafael Lemos <contact@flemosr.com>
2025-11-03 16:41:36 -0300

    lnm-sdk: adjust `lib.rs` API

--- lnm-sdk/src/lib.rs
+++ lnm-sdk/src/lib.rs
@@ -1,30 +1,43 @@
-pub mod api;
+mod api;
+
+pub use api::{ApiContext, ApiContextConfig, websocket::state::ConnectionStatus};
 
 pub mod error {
-    pub use super::api::rest::{
-        error::RestApiError,
-        models::error::{
-            BoundedPercentageValidationError, FuturesTradeRequestValidationError,
-            LeverageValidationError, LowerBoundedPercentageValidationError, MarginValidationError,
-            PriceValidationError, QuantityValidationError, TradeValidationError, ValidationError,
+    pub use super::api::{
+        rest::{
+            error::RestApiError,
+            models::error::{
+                BoundedPercentageValidationError, FuturesTradeRequestValidationError,
+                LeverageValidationError, LowerBoundedPercentageValidationError,
+                MarginValidationError, PriceValidationError, QuantityValidationError,
+                TradeValidationError, ValidationError,
+            },
         },
+        websocket::error::{WebSocketApiError, WebSocketConnectionError},
     };
 }
 
 pub mod models {
     pub use uuid::Uuid;
 
-    pub use super::api::rest::models::{
-        leverage::Leverage,
-        margin::Margin,
-        price::{BoundedPercentage, LowerBoundedPercentage, Price},
-        price_history::PriceEntryLNM,
-        quantity::Quantity,
-        ticker::Ticker,
-        trade::{
-            LnmTrade, Trade, TradeClosed, TradeExecution, TradeExecutionType, TradeRunning,
-            TradeSide, TradeSize, TradeStatus, util as trade_util,
+    pub use super::api::{
+        rest::models::{
+            leverage::Leverage,
+            margin::Margin,
+            price::{BoundedPercentage, LowerBoundedPercentage, Price},
+            price_history::PriceEntryLNM,
+            quantity::Quantity,
+            ticker::Ticker,
+            trade::{
+                LnmTrade, Trade, TradeClosed, TradeExecution, TradeExecutionType, TradeRunning,
+                TradeSide, TradeSize, TradeStatus, util as trade_util,
+            },
+            user::{User, UserRole},
         },
-        user::{User, UserRole},
+        websocket::models::{
+            JsonRpcResponse, LastTickDirection, LnmJsonRpcReqMethod, LnmJsonRpcRequest,
+            LnmJsonRpcResponse, LnmWebSocketChannel, PriceIndexLNM, PriceTickLNM, SubscriptionData,
+            WebSocketUpdate,
+        },
     };
 }
