6e1d22a8da8a4a07700d21da668711a855f15b9e
Rafael Lemos <contact@flemosr.com>
2025-11-04 12:16:55 -0300

    quantoxide: adjust `lib.rs` API, WIP

--- quantoxide/src/lib.rs
+++ quantoxide/src/lib.rs
@@ -1,9 +1,9 @@
-mod db;
+pub mod db;
 pub(crate) mod indicators;
 pub mod signal;
 pub mod sync;
-pub mod trade;
-pub mod tui;
+pub mod trade; // WIP
+pub mod tui; // TODO
 pub(crate) mod util;
 
 pub mod error {
@@ -20,6 +20,19 @@ pub mod error {
             error::{SyncProcessFatalError, SyncProcessRecoverableError},
             real_time_collection_task::error::RealTimeCollectionError,
             sync_price_history_task::error::SyncPriceHistoryError,
+        },
+    };
+    pub use super::trade::{
+        error::{TradeCoreError, TradeExecutorError},
+        live::{
+            error::LiveError,
+            executor::error::{
+                ExecutorActionError, ExecutorProcessFatalError, ExecutorProcessRecoverableError,
+                LiveTradeExecutorError,
+            },
+            process::error::{
+                LiveProcessError, LiveProcessFatalError, LiveProcessRecoverableError,
+            },
         },
     };
     pub use super::util::PanicPayload;
