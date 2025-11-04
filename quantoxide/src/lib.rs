84d63fbe02ac3788a9ecde1c7b67a69ee5564e8f
Rafael Lemos <contact@flemosr.com>
2025-11-03 21:08:19 -0300

    quantoxide: adjust `lib.rs` API, WIP

--- quantoxide/src/lib.rs
+++ quantoxide/src/lib.rs
@@ -1,13 +1,15 @@
-pub mod db;
-
-pub mod sync;
-
+mod db;
+pub mod indicators;
 pub mod signal;
-
+pub mod sync;
 pub mod trade;
-
+pub mod tui;
 pub mod util;
 
-pub mod indicators;
+pub mod error {
+    pub use super::db::error::DbError;
+}
 
-pub mod tui;
+pub mod models {
+    pub use super::db::models::{PriceHistoryEntry, PriceHistoryEntryLOCF, PriceTick};
+}
