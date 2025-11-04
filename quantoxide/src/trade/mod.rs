4697a99b7569f3186b0c64c9ddf18286713907c4
Rafael Lemos <contact@flemosr.com>
2025-11-04 12:07:55 -0300

    quantoxide: adjust `trade` API

--- quantoxide/src/trade/mod.rs
+++ quantoxide/src/trade/mod.rs
@@ -2,9 +2,24 @@ mod core;
 pub(crate) mod error;
 
 pub mod backtest;
-pub mod live;
+pub(crate) mod live;
 
 pub use core::{
     ClosedTradeHistory, RawOperator, RunningTradesMap, SignalOperator, Stoploss, TradeExecutor,
     TradeExt, TradeTrailingStoploss, TradingState,
+};
+pub use live::{
+    config::{LiveConfig, LiveTradeExecutorConfig},
+    engine::{LiveController, LiveEngine},
+    executor::{
+        LiveTradeExecutor, LiveTradeExecutorLauncher,
+        state::{
+            LiveTradeExecutorStatus, LiveTradeExecutorStatusNotReady,
+            live_trading_session::TradingSessionRefreshOffset,
+        },
+        update::{
+            LiveTradeExecutorReceiver, LiveTradeExecutorUpdate, LiveTradeExecutorUpdateOrder,
+        },
+    },
+    state::{LiveReader, LiveReceiver, LiveStatus, LiveUpdate},
 };
