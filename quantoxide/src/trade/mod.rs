102bf8615cebc73c9e7cabd4ce6f6955566b22a9
Rafael Lemos <contact@flemosr.com>
2025-11-04 11:16:24 -0300

    quantoxide: adjust `trade` API, WIP

--- quantoxide/src/trade/mod.rs
+++ quantoxide/src/trade/mod.rs
@@ -1,5 +1,10 @@
-pub mod core;
-pub mod error;
+mod core;
+pub(crate) mod error;
 
 pub mod backtest;
 pub mod live;
+
+pub use core::{
+    ClosedTradeHistory, RawOperator, RunningTradesMap, SignalOperator, Stoploss, TradeExecutor,
+    TradeExt, TradeTrailingStoploss, TradingState,
+};
