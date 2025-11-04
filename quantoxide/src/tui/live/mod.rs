cfaf2d168f6d944f626e72eb2338f96be494b40e
Rafael Lemos <contact@flemosr.com>
2025-11-04 12:14:55 -0300

    quantoxide: adjust `tui::live` imports

--- quantoxide/src/tui/live/mod.rs
+++ quantoxide/src/tui/live/mod.rs
@@ -10,13 +10,7 @@ use tokio::{
 };
 
 use crate::{
-    trade::{
-        core::ClosedTradeHistory,
-        live::{
-            engine::LiveEngine,
-            state::{LiveReceiver, LiveUpdate},
-        },
-    },
+    trade::{ClosedTradeHistory, LiveEngine, LiveReceiver, LiveUpdate},
     util::AbortOnDropHandle,
 };
 
