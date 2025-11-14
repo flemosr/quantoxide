c7b7a2076fea94bc5ff04c837d014fcb4a4d8c81
Rafael Lemos <contact@flemosr.com>
2025-11-13 21:26:15 -0300

    lnm-sdk: add `RestPathV3::FuturesDataTicker` variant

--- lnm-sdk/src/api_v3/rest/lnm/path.rs
+++ lnm-sdk/src/api_v3/rest/lnm/path.rs
@@ -3,12 +3,14 @@ use crate::shared::rest::lnm::base::RestPath;
 #[derive(Clone)]
 pub(in crate::api_v3) enum RestPathV3 {
     FuturesIsolatedTrade,
+    FuturesDataTicker,
 }
 
 impl RestPath for RestPathV3 {
     fn to_path_string(self) -> String {
         match self {
             RestPathV3::FuturesIsolatedTrade => "/v3/futures/isolated/trade".into(),
+            RestPathV3::FuturesDataTicker => "/v3/futures/ticker".into(),
         }
     }
 }
