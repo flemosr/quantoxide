a2238ffedbc730c34cec89aa964d70b4ef2db106
Rafael Lemos <contact@flemosr.com>
2025-11-14 12:29:30 -0300

    lnm-sdk: add `RestPathV3::FuturesIsolatedTradesCancelAll` variant

--- lnm-sdk/src/api_v3/rest/lnm/path.rs
+++ lnm-sdk/src/api_v3/rest/lnm/path.rs
@@ -3,6 +3,7 @@ use crate::shared::rest::lnm::base::RestPath;
 #[derive(Clone)]
 pub(in crate::api_v3) enum RestPathV3 {
     FuturesIsolatedTrade,
+    FuturesIsolatedTradesCancelAll,
     FuturesDataTicker,
 }
 
@@ -10,6 +11,9 @@ impl RestPath for RestPathV3 {
     fn to_path_string(self) -> String {
         match self {
             RestPathV3::FuturesIsolatedTrade => "/v3/futures/isolated/trade".into(),
+            RestPathV3::FuturesIsolatedTradesCancelAll => {
+                "/v3/futures/isolated/trades/cancel-all".into()
+            }
             RestPathV3::FuturesDataTicker => "/v3/futures/ticker".into(),
         }
     }
