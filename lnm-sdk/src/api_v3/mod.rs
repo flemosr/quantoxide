b57d27eebff62e65e5558967994efeffca5af1f8
Rafael Lemos <contact@flemosr.com>
2025-11-24 20:35:19 -0300

    lnm-sdk: adjust `api_v3` API

--- lnm-sdk/src/api_v3/mod.rs
+++ lnm-sdk/src/api_v3/mod.rs
@@ -47,7 +47,7 @@ pub mod models {
         account::Account,
         cross_leverage::CrossLeverage,
         funding::{CrossFunding, FundingSettlement, IsolatedFunding},
-        futures_data::OhlcCandle,
+        futures_data::{OhlcCandle, OhlcRange},
         oracle::{Index, LastPrice},
         page::Page,
         trade::{CrossOrder, CrossPosition, Trade},
