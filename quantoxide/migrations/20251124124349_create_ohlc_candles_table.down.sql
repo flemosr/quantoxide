6d7ccab0673000aa8c289bf6dc9d68868b81e39e
Rafael Lemos <contact@flemosr.com>
2025-11-26 11:50:40 -0300

    quantoxide: adjust `create_ohlc_candles_table` sqlx migration

--- quantoxide/migrations/20251124124349_create_ohlc_candles_table.down.sql
+++ quantoxide/migrations/20251124124349_create_ohlc_candles_table.down.sql
@@ -1,1 +1,3 @@
+DROP TRIGGER IF EXISTS trigger_ohlc_candles_updated_at ON ohlc_candles;
+DROP FUNCTION IF EXISTS update_ohlc_candles_updated_at();
 DROP TABLE ohlc_candles;
--- quantoxide/migrations/20251124124349_create_ohlc_candles_table.up.sql
+++ quantoxide/migrations/20251124124349_create_ohlc_candles_table.up.sql
@@ -6,8 +6,23 @@ CREATE TABLE ohlc_candles (
     close DOUBLE PRECISION NOT NULL,
     volume BIGINT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-    gap BOOLEAN NOT NULL DEFAULT FALSE
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+    gap BOOLEAN NOT NULL DEFAULT FALSE,
+    stable BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE INDEX idx_ohlc_candles_gaps ON ohlc_candles (gap)
 WHERE gap IS TRUE;
+
+CREATE OR REPLACE FUNCTION update_ohlc_candles_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_ohlc_candles_updated_at
+BEFORE UPDATE ON ohlc_candles
+FOR EACH ROW
+EXECUTE FUNCTION update_ohlc_candles_updated_at();
