24a31273af47bde91abc4f03e76ea3e4e8b1ab7d
Rafael Lemos <contact@flemosr.com>
2025-12-04 11:23:39 -0300

    quantoxide: remove `*PriceEntryLOCF` models

--- quantoxide/src/db/models.rs
+++ quantoxide/src/db/models.rs
@@ -6,21 +6,6 @@ use uuid::Uuid;
 
 use crate::util::DateTimeExt;
 
-#[derive(Debug, FromRow, Clone)]
-pub struct PriceEntryLOCF {
-    pub time: DateTime<Utc>,
-    pub value: f64,
-    pub ma_5: Option<f64>,
-    pub ma_60: Option<f64>,
-    pub ma_300: Option<f64>,
-}
-
-#[derive(Debug, FromRow, Clone)]
-pub(crate) struct PartialPriceEntryLOCF {
-    pub time: DateTime<Utc>,
-    pub value: f64,
-}
-
 #[derive(Debug, Clone, FromRow, PartialEq)]
 pub struct PriceTickRow {
     pub time: DateTime<Utc>,
