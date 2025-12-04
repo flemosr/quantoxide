3378595b6d1225a0266b9be2e20828facf6149ef
Rafael Lemos <contact@flemosr.com>
2025-12-04 11:20:57 -0300

    quantoxide: refactor `Database`

--- quantoxide/src/db/mod.rs
+++ quantoxide/src/db/mod.rs
@@ -2,21 +2,22 @@ use std::sync::Arc;
 
 use sqlx::postgres::PgPoolOptions;
 
-use crate::db::{postgres::ohlc_candles::PgOhlcCandlesRepo, repositories::OhlcCandlesRepository};
-
 pub(crate) mod error;
 pub(crate) mod models;
 mod postgres;
 mod repositories;
 
 use error::{DbError, Result};
-use postgres::{price_ticks::PgPriceTicksRepo, running_trades::PgRunningTradesRepo};
-use repositories::{PriceTicksRepository, RunningTradesRepository};
+use postgres::{
+    ohlc_candles::PgOhlcCandlesRepo, price_ticks::PgPriceTicksRepo,
+    running_trades::PgRunningTradesRepo,
+};
+use repositories::{OhlcCandlesRepository, PriceTicksRepository, RunningTradesRepository};
 
 pub struct Database {
+    pub(crate) ohlc_candles: Box<dyn OhlcCandlesRepository>,
     pub(crate) price_ticks: Box<dyn PriceTicksRepository>,
     pub(crate) running_trades: Box<dyn RunningTradesRepository>,
-    pub(crate) ohlc_candles: Box<dyn OhlcCandlesRepository>,
 }
 
 impl Database {
@@ -33,14 +34,14 @@ impl Database {
             .map_err(DbError::Migration)?;
 
         let pool = Arc::new(pool);
+        let ohlc_candles = Box::new(PgOhlcCandlesRepo::new(pool.clone()));
         let price_ticks = Box::new(PgPriceTicksRepo::new(pool.clone()));
         let running_trades = Box::new(PgRunningTradesRepo::new(pool.clone()));
-        let ohlc_candles = Box::new(PgOhlcCandlesRepo::new(pool.clone()));
 
         Ok(Arc::new(Self {
+            ohlc_candles,
             price_ticks,
             running_trades,
-            ohlc_candles,
         }))
     }
 }
