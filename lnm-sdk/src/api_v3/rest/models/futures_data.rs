f1e3754ab1a16bd62dfb9a4804726511ea7ce209
Rafael Lemos <contact@flemosr.com>
2025-12-09 18:42:47 -0300

    lnm-sdk: improve `OhlcCandle` docs

--- lnm-sdk/src/api_v3/rest/models/futures_data.rs
+++ lnm-sdk/src/api_v3/rest/models/futures_data.rs
@@ -5,6 +5,9 @@ use serde::Deserialize;
 
 use crate::shared::models::price::Price;
 
+/// Time range for OHLC (Open-High-Low-Close) candles.
+///
+/// Specifies the duration of each candlestick when querying historical price data.
 pub enum OhlcRange {
     OneMinute,
     ThreeMinutes,
@@ -47,6 +50,32 @@ impl fmt::Display for OhlcRange {
     }
 }
 
+/// OHLC (Open-High-Low-Close) candlestick data.
+///
+/// Represents price and volume data for a specific time period.
+///
+/// # Examples
+///
+/// ```no_run
+/// # async fn example(rest_api: lnm_sdk::api_v3::RestClient) -> Result<(), Box<dyn std::error::Error>> {
+/// use lnm_sdk::api_v3::models::{OhlcCandle, OhlcRange, Page};
+///
+/// let candles: Page<OhlcCandle> = rest_api
+///     .futures_data
+///     .get_candles(None, None, None, Some(OhlcRange::OneHour), None)
+///     .await?;
+///
+/// for candle in candles.data() {
+///     println!("Time: {}", candle.time());
+///     println!("Open: {}", candle.open());
+///     println!("High: {}", candle.high());
+///     println!("Low: {}", candle.low());
+///     println!("Close: {}", candle.close());
+///     println!("Volume: {}", candle.volume());
+/// }
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Deserialize, Debug, Clone)]
 pub struct OhlcCandle {
     time: DateTime<Utc>,
