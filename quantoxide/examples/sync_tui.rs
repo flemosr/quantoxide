57a5f24513d7cbd0791603a6a7a6822f7bd082c7
Rafael Lemos <contact@flemosr.com>
2025-12-16 07:53:06 -0300

    quantoxide: adjust `examples/sync_tui`, add custom config

--- quantoxide/examples/sync_tui.rs
+++ quantoxide/examples/sync_tui.rs
@@ -29,7 +29,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .log("Database ready. Initializing `SyncEngine`...".into())
         .await?;
 
-    let sync_engine = SyncEngine::new(SyncConfig::default(), db, domain, SyncMode::Backfill)?;
+    let config = SyncConfig::default();
+    // How far back to fetch price history data can be configured with:
+    // let config = config.with_price_history_reach(180.try_into()?); // 180 days
+
+    let sync_engine = SyncEngine::new(config, db, domain, SyncMode::Backfill)?;
 
     sync_tui
         .log("Initialization OK. Coupling `SyncEngine`...".into())
