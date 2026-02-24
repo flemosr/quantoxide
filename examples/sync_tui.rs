45ddd754f9eb8a1fb232601f6e4457cda957c2d2
Rafael Lemos <contact@flemosr.com>
2026-02-24 12:17:04 -0300

    adjust `examples::sync_tui`, adjust `config`

--- examples/sync_tui.rs
+++ examples/sync_tui.rs
@@ -32,7 +32,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let config = SyncConfig::default();
     // How far back to fetch price history data can be configured with:
-    // let config = config.with_price_history_reach(180.try_into()?); // 180 days
+    // let config = config
+    //     .with_price_history_reach(date) // or:
+    //     .with_price_history_reach_max();
 
     let sync_engine = SyncEngine::new(config, db, domain, SyncMode::Backfill)?;
 
