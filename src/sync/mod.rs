4e51924995e220ddc35203ff411c3f29b074dc5c
Rafael Lemos <contact@flemosr.com>
2026-02-24 10:35:42 -0300

    adjust `sync` mod exports

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -6,5 +6,11 @@ mod state;
 
 pub use config::SyncConfig;
 pub use engine::{SyncController, SyncEngine, SyncMode};
+pub use process::sync_funding_settlements_task::{
+    LNM_SETTLEMENT_A_END, LNM_SETTLEMENT_A_START, LNM_SETTLEMENT_B_END, LNM_SETTLEMENT_B_START,
+    LNM_SETTLEMENT_C_START, LNM_SETTLEMENT_INTERVAL_8H, LNM_SETTLEMENT_INTERVAL_DAY,
+    funding_settlements_state::FundingSettlementsState,
+};
+pub use process::sync_price_history_task::LNM_OHLC_CANDLE_START;
 pub use process::sync_price_history_task::price_history_state::PriceHistoryState;
 pub use state::{SyncReader, SyncReceiver, SyncStatus, SyncStatusNotSynced, SyncUpdate};
